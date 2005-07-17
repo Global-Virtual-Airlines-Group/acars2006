b3afe51dca68b327cc49f6ce63742b5b6fff015e
Luke Kolin <luke@deltava.org>
2005-07-17 20:06:20 +0000

    Added servinfo-format data feed for web site
    git-svn-id: file:///home/svn/acars2006@83 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/beans/ACARSConnectionPool.java
+++ src/java/org/deltava/acars/beans/ACARSConnectionPool.java
@@ -7,7 +7,11 @@ import java.nio.channels.*;
 import org.apache.log4j.Logger;
 
 import org.deltava.acars.ACARSException;
+
 import org.deltava.beans.Pilot;
+import org.deltava.beans.servinfo.*;
+
+import org.deltava.acars.message.*;
 
 /**
  * A TCP/IP Connection Pool.
@@ -16,75 +20,117 @@ import org.deltava.beans.Pilot;
  * @since 1.0
  */
 
-public class ACARSConnectionPool {
-	
+public class ACARSConnectionPool implements ServInfoProvider {
+
 	private static final Logger log = Logger.getLogger(ACARSConnectionPool.class);
-	
+
 	// Hard-coded anonymous inactivity timeout (in ms)
 	private static final long ANONYMOUS_INACTIVITY_TIMEOUT = 15000;
 
 	// List of connections, disconnected connections and connection pool info
-	private int maxSize;
-	private List cons;
-	private List disCon;
-	
+	private int _maxSize;
+	private List _cons;
+	private List _disCon;
+
 	// Inactivity timeout
-	private long inactivityTimeout = -1;
-	
+	private long _inactivityTimeout = -1;
+
 	// The selector to use for non-blocking I/O reads
-	private Selector cSelector;
-	
+	private Selector _cSelector;
+
 	// The input/output stacks in raw text
-	private MessageStack inputStack;
-	private MessageStack outputStack;
-	
+	private MessageStack _inputStack;
+	private MessageStack _outputStack;
+
 	public ACARSConnectionPool(int mxSize) {
 		super();
-		
+
 		// Init the maximum size and connection lists
-		this.maxSize = (mxSize > 0) ? mxSize : -1;
-		this.cons = new ArrayList();
-		this.disCon = new ArrayList();
+		_maxSize = (mxSize > 0) ? mxSize : -1;
+		_cons = new ArrayList();
+		_disCon = new ArrayList();
 	}
-	
+
+	public NetworkInfo getNetworkInfo() {
+		NetworkInfo info = new NetworkInfo("ACARS");
+		info.setVersion("1");
+		info.setValidDate(new Date());
+		for (Iterator i = _cons.iterator(); i.hasNext();) {
+			ACARSConnection con = (ACARSConnection) i.next();
+			PositionMessage pos = (PositionMessage) con.getInfo(ACARSConnection.POSITION_INFO);
+			InfoMessage usrInfo = (InfoMessage) con.getInfo(ACARSConnection.FLIGHT_INFO);
+
+			// Add Pilots to servinfo data, if they're logged in and have info/position data
+			if (con.isAuthenticated() && (pos != null) && (usrInfo != null)) {
+				Pilot usr = con.getUser();
+
+				// Build the pilot object
+				org.deltava.beans.servinfo.Pilot p = new org.deltava.beans.servinfo.Pilot(usr.getID());
+				p.setName(usr.getName());
+				p.setPilotID(usr.getID());
+
+				// Pass position information
+				p.setAltitude(pos.getAltitude());
+				p.setGroundSpeed(pos.getGspeed());
+				p.setPosition(pos.getLatitude(), pos.getLongitude());
+
+				// Pass back user info
+				p.setAirportD(usrInfo.getAirportD());
+				p.setAirportA(usrInfo.getAirportA());
+				p.setCallsign(usrInfo.getFlightCode());
+				p.setEquipmentCode(usrInfo.getEquipmentType());
+				p.setComments(usrInfo.getComments());
+
+				// Add the pilot object
+				info.add(p);
+			}
+		}
+
+		return info;
+	}
+
+	public NetworkStatus getNetworkStatus() {
+		return new NetworkStatus("ACARS");
+	}
+
 	public void add(ACARSConnection c) throws ACARSException {
-		
+
 		// Check if we're already there
-		if (cons.contains(c))
-			throw new ACARSException("Connection already in pool");		
-		
-		if (cons.size() >= maxSize)
+		if (_cons.contains(c))
+			throw new ACARSException("Connection already in pool");
+
+		if (_cons.size() >= _maxSize)
 			throw new ACARSException("Connection Pool full");
-			
+
 		// Register the SocketChannel with the selector
 		try {
-			c.getChannel().register(cSelector, SelectionKey.OP_READ);
-			cons.add(c);
+			c.getChannel().register(_cSelector, SelectionKey.OP_READ);
+			_cons.add(c);
 		} catch (ClosedChannelException cce) {
 			throw new ACARSException(cce.getMessage());
 		}
 	}
-	
+
 	public Collection checkConnections() {
-		
+
 		// Start with the list of dropped connections
-		ArrayList disCons = new ArrayList(this.disCon);
-		this.disCon.clear();
-		
+		ArrayList disCons = new ArrayList(_disCon);
+		_disCon.clear();
+
 		// Build list of dropped connections; return it with just the dropped connections if we have no timeout
-		if (this.inactivityTimeout == -1)
+		if (_inactivityTimeout == -1)
 			return disCons;
-		
+
 		// Get current time to save repeated system calls
 		long now = System.currentTimeMillis();
-		
+
 		// Loop through the channels
-		for (Iterator i = cons.iterator(); i.hasNext(); ) {
+		for (Iterator i = _cons.iterator(); i.hasNext();) {
 			ACARSConnection con = (ACARSConnection) i.next();
-			
+
 			// Calculate the inactivity timeout
-			long timeout = con.isAuthenticated() ? this.inactivityTimeout : ANONYMOUS_INACTIVITY_TIMEOUT;
-			
+			long timeout = con.isAuthenticated() ? _inactivityTimeout : ANONYMOUS_INACTIVITY_TIMEOUT;
+
 			// Have we exceeded the timeout interval
 			if ((now - con.getLastActivity()) > timeout) {
 				con.close();
@@ -92,167 +138,171 @@ public class ACARSConnectionPool {
 				disCons.add(con);
 			}
 		}
-		
+
 		// Return the list of dropped connections
 		return disCons;
 	}
-	
+
 	public boolean hasConnection(String remoteAddr) {
-		
+
 		// Loop through the connections
-		Iterator i = this.cons.iterator();
+		Iterator i = _cons.iterator();
 		while (i.hasNext()) {
 			ACARSConnection c = (ACARSConnection) i.next();
 			if (c.getRemoteAddr().equals(remoteAddr))
 				return true;
 		}
-		
+
 		// No connection from that address found, return false
 		return false;
 	}
-	
+
 	public Collection getAll() {
-		return cons;
+		return _cons;
 	}
-	
+
 	public ACARSConnection get(long cid) {
-		for (Iterator i = cons.iterator(); i.hasNext(); ) {
+		for (Iterator i = _cons.iterator(); i.hasNext();) {
 			ACARSConnection c = (ACARSConnection) i.next();
-			if (c.equals(cid)) return c;
+			if (c.equals(cid))
+				return c;
 		}
-		
+
 		// Return nothing if not found
 		return null;
 	}
-	
+
 	public Collection get(String pid) {
-		
+
 		// Wildcard matches everyone
 		if (("*".equals(pid)) || (pid == null))
-			return cons;
+			return _cons;
 
 		// Build results
 		Set results = new HashSet();
-		for (Iterator i = cons.iterator(); i.hasNext(); ) {
+		for (Iterator i = _cons.iterator(); i.hasNext();) {
 			ACARSConnection c = (ACARSConnection) i.next();
 			if (c.getUserID().equals(pid))
 				results.add(c);
 		}
-		
+
 		return results;
 	}
-	
+
 	public Iterator get(Pilot userInfo) {
-		
+
 		// Build results list
 		ArrayList results = new ArrayList();
-		
+
 		// Loop through the connections
-		for (Iterator i = cons.iterator(); i.hasNext(); ) {
+		for (Iterator i = _cons.iterator(); i.hasNext();) {
 			ACARSConnection c = (ACARSConnection) i.next();
 			if ((c.isAuthenticated()) && (c.getUser().equals(userInfo)))
 				results.add(c);
 		}
-		
+
 		// Return the iterator
 		return results.iterator();
 	}
-	
+
 	public ACARSConnection get(SocketChannel ch) {
-		
+
 		// Loop through the connections
-		Iterator i = cons.iterator();
+		Iterator i = _cons.iterator();
 		while (i.hasNext()) {
 			ACARSConnection c = (ACARSConnection) i.next();
-			if (c.equals(ch)) return c;
+			if (c.equals(ch))
+				return c;
 		}
-		
+
 		// Return nothing if not found
 		return null;
 	}
-	
+
 	public void read() {
-		
+
 		// Get the list of channels waiting for input
-		for (Iterator i = cSelector.selectedKeys().iterator(); i.hasNext(); ) {
+		for (Iterator i = _cSelector.selectedKeys().iterator(); i.hasNext();) {
 			SelectionKey sKey = (SelectionKey) i.next();
-			
+
 			// If the selection key is ready for reading, get the Connection and read
 			if (sKey.isReadable()) {
 				ACARSConnection con = get((SocketChannel) sKey.channel());
-				
+
 				try {
 					String msg = con.read();
 					if (msg != null) {
 						Envelope env = new Envelope(con.getUser(), msg, con.getID());
-						inputStack.push(env);
+						_inputStack.push(env);
 					}
 				} catch (SocketException se) {
 					log.error(se.getMessage(), se);
 					con.close();
-					cons.remove(con);
+					_cons.remove(con);
 				} catch (ProtocolException pe) {
 					con.close();
-					cons.remove(con);
-					disCon.add(con);
+					_cons.remove(con);
+					_disCon.add(con);
 				}
 			}
-			
+
 			// Remove from the selected keys list
 			i.remove();
 		}
 	}
-	
+
 	public void remove(String id) {
-		
-		for (Iterator i = cons.iterator(); i.hasNext(); ) {
+
+		for (Iterator i = _cons.iterator(); i.hasNext();) {
 			ACARSConnection c = (ACARSConnection) i.next();
-			
+
 			// Close/Remove if found
 			if (c.equals(id)) {
-				if (c.isConnected()) c.close();
+				if (c.isConnected())
+					c.close();
 				i.remove();
 				break;
 			}
 		}
 	}
-	
+
 	public void remove(ACARSConnection c) {
-		
+
 		// Find the connection
-		int pos = cons.indexOf(c);
+		int pos = _cons.indexOf(c);
 		if (pos != -1) {
-			if (c.isConnected()) c.close();
-			cons.remove(pos);
+			if (c.isConnected())
+				c.close();
+			_cons.remove(pos);
 		}
 	}
-	
+
 	public void setSelector(Selector cs) {
-		cSelector = cs;
+		_cSelector = cs;
 	}
-	
+
 	public void setStacks(MessageStack is, MessageStack os) throws ACARSException {
-		
+
 		// Check that the stacks are not null
 		if ((is == null) || (os == null))
 			throw new ACARSException("Input/Output stacks are NULL");
-		
+
 		// Check that we're not resetting anything
-		if ((inputStack == null) && (outputStack == null)) {
-			inputStack = is;
-			outputStack = os;
+		if ((_inputStack == null) && (_outputStack == null)) {
+			_inputStack = is;
+			_outputStack = os;
 		}
 	}
-	
+
 	public void setTimeout(int toSeconds) {
-		inactivityTimeout = (toSeconds < 60) ? -1 : (toSeconds * 1000); 
+		_inactivityTimeout = (toSeconds < 60) ? -1 : (toSeconds * 1000);
 	}
-	
+
 	public void write() {
 		// Loop through the raw output stack
-		while (outputStack.hasNext()) {
-			Envelope env = outputStack.pop();
-			
+		while (_outputStack.hasNext()) {
+			Envelope env = _outputStack.pop();
+
 			// Get the connection and write the message
 			ACARSConnection c = get(env.getConnectionID());
 			c.write((String) env.getMessage());
