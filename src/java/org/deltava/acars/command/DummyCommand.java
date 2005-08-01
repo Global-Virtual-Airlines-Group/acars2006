52b70b0f9fac8d10e00c0b94c74ffc93eec4a281
Luke Kolin <luke@deltava.org>
2005-08-01 20:55:53 +0000

    Added Dummy command and optional ACK acknowledging
    git-svn-id: file:///home/svn/acars2006@351 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/acars/command/DummyCommand.java
@@ -0,0 +1,22 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.acars.command;
+
+import org.deltava.acars.beans.Envelope;
+
+/**
+ * An ACARS command to swallow messages.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+public class DummyCommand implements ACARSCommand {
+
+   /**
+    * Executes the command.
+    * @param ctx the Command context
+    * @param env the Message envelope
+    */
+   public void execute(CommandContext ctx, Envelope env) {
+      // NOOP
+   }
+}
