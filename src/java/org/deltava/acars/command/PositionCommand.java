dbdb17bc5cafafe69e0f33e692662da7e4ed70ad
Luke Kolin <luke@deltava.org>
2005-08-01 04:08:04 +0000

    Build 42 updates
    git-svn-id: file:///home/svn/acars2006@344 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/PositionCommand.java
+++ src/java/org/deltava/acars/command/PositionCommand.java
@@ -41,11 +41,16 @@ public class PositionCommand implements ACARSCommand {
 		}
 
 		// Get the last position report and its age
+		InfoMessage info = (InfoMessage) con.getInfo(ACARSConnection.FLIGHT_INFO);
 		PositionMessage oldPM = (PositionMessage) con.getInfo(ACARSConnection.POSITION_INFO);
-		long pmAge = System.currentTimeMillis() - ((oldPM == null) ? 0 : oldPM.getTime()); 
+		long pmAge = System.currentTimeMillis() - ((oldPM == null) ? 0 : oldPM.getTime());
+		
+		// If we are an offline fight, update the timestamp of the mesage
+		if (info.isOffline())
+			msg.setTime(msg.getDate().getTime());
 
 		// Set the info for the connection and write it to the database if we aren't being flooded
-		if (pmAge >= SystemData.getInt("acars.position_interval")) {
+		if ((!info.isOffline()) || (pmAge >= SystemData.getInt("acars.position_interval"))) {
 			con.setInfo(msg);
 			log.debug("Received position from " + con.getUser().getPilotCode() + " (" + con.getFormatID() + ")");
 			
