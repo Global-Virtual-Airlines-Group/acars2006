486762b803d38cc97e0d7f836702a452f7b929b8
Luke Kolin <luke@deltava.org>
2005-08-02 05:45:41 +0000

    Offline PIREP support fixes
    git-svn-id: file:///home/svn/acars2006@367 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/InfoCommand.java
+++ src/java/org/deltava/acars/command/InfoCommand.java
@@ -11,8 +11,6 @@ import org.deltava.acars.message.*;
 import org.deltava.dao.DAOException;
 import org.deltava.dao.acars.SetInfo;
 
-import org.deltava.util.system.SystemData;
-
 /**
  * An ACARS Command to log Flight data.
  * @author Luke
@@ -21,7 +19,7 @@ import org.deltava.util.system.SystemData;
  */
 
 public class InfoCommand implements ACARSCommand {
-	
+
 	private static final Logger log = Logger.getLogger(InfoCommand.class);
 
 	/**
@@ -30,31 +28,30 @@ public class InfoCommand implements ACARSCommand {
 	 * @param env the message Envelope
 	 */
 	public void execute(CommandContext ctx, Envelope env) {
-		
+
 		// Get the message
 		InfoMessage msg = (InfoMessage) env.getMessage();
-		
+		String flightType = msg.isOffline() ? "Offline" : "Online";
+
 		// Write the info to the database
 		try {
 			Connection c = ctx.getConnection();
 			SetInfo infoDAO = new SetInfo(c);
 			infoDAO.write(msg, env.getConnectionID());
-			
-			// Create the ack message and envelope
-			if (SystemData.getBoolean("acars.ack.info")) {
-				AcknowledgeMessage ackMsg = new AcknowledgeMessage(env.getOwner(), msg.getID());
-				ackMsg.setEntry("flight_id", String.valueOf(msg.getFlightID()));
-				ctx.push(ackMsg, env.getConnectionID());						
-			}
+
+			// Create the ack message and envelope - these are always acknowledged
+			AcknowledgeMessage ackMsg = new AcknowledgeMessage(env.getOwner(), msg.getID());
+			ackMsg.setEntry("flight_id", String.valueOf(msg.getFlightID()));
+			ctx.push(ackMsg, env.getConnectionID());
 		} catch (DAOException de) {
 			log.error(de.getMessage(), de);
 		} finally {
 			ctx.release();
 		}
-		
+
 		// Set the info for the connection and write it to the database
 		ACARSConnection con = ctx.getACARSConnection();
 		con.setInfo(msg);
-		log.debug("Received info from " + con.getUserID() + " (" + con.getFormatID() + ")");
+		log.info("Received " + flightType + " flight information from " + con.getUserID());
 	}
 }
