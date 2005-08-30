275bfac012bb27344cdcc8578ef175cdc7cdd044
Luke Kolin <luke@deltava.org>
2005-08-30 20:17:04 +0000

    End flight if not closed already
    git-svn-id: file:///home/svn/acars2006@670 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/QuitCommand.java
+++ src/java/org/deltava/acars/command/QuitCommand.java
@@ -1,10 +1,16 @@
 // Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
 package org.deltava.acars.command;
 
-import org.deltava.acars.beans.Envelope;
+import java.util.Date;
+import java.sql.Connection;
 
-import org.deltava.acars.message.DataMessage;
-import org.deltava.acars.message.DataResponseMessage;
+import org.apache.log4j.Logger;
+
+import org.deltava.acars.beans.*;
+import org.deltava.acars.message.*;
+
+import org.deltava.dao.DAOException;
+import org.deltava.dao.acars.SetInfo;
 
 /**
  * @author Luke
@@ -13,6 +19,8 @@ import org.deltava.acars.message.DataResponseMessage;
  */
 
 public class QuitCommand implements ACARSCommand {
+   
+   private static final Logger log = Logger.getLogger(QuitCommand.class);
 	
 	/**
 	 * Executes the command.
@@ -20,6 +28,28 @@ public class QuitCommand implements ACARSCommand {
 	 * @param env the message Envelope
 	 */
 	public void execute(CommandContext ctx, Envelope env) {
+	   
+	   // Get the ACARS connection and flight information
+	   ACARSConnection ac = ctx.getACARSConnection();
+	   InfoMessage info = ac.getFlightInfo();
+	   
+		// Mark the flight as closed
+	   if ((info != null) && (info.getEndTime() == null)) {
+	      info.setEndTime(new Date());
+
+			try {
+				Connection c = ctx.getConnection();
+				SetInfo infoDAO = new SetInfo(c);
+				infoDAO.close(info.getFlightID(), env.getConnectionID(), true);
+			} catch (DAOException de) {
+				log.error(de.getMessage(), de);
+			} finally {
+				ctx.release();
+			}
+			
+			// Clear flight info and log
+			log.info("Flight Completed by " + ac.getUserID());
+	   }
 
 		// Create a deletepilots message
 		DataResponseMessage msg = new DataResponseMessage(env.getOwner(), DataMessage.REQ_REMOVEUSER);
