66392ddd2f15ffd2231c10475b4a4f9684f3c83d
Luke Kolin <luke@deltava.org>
2005-10-08 15:31:31 +0000

    Add debug connection dump
    git-svn-id: file:///home/svn/acars2006@1280 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/acars/beans/ACARSDebugConnection.java
@@ -0,0 +1,78 @@
+// Copyright (c) 2005 Delta Virtual Airlines. All Rights Reserved.
+package org.deltava.acars.beans;
+
+import java.io.*;
+import java.net.*;
+
+import java.nio.channels.SocketChannel;
+
+/**
+ * An ACARS Connection that dumps messages to a text file.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+public class ACARSDebugConnection extends ACARSConnection {
+
+	private PrintWriter _debugWriter;
+
+	public ACARSDebugConnection(long cid, SocketChannel sc) {
+		super(cid, sc);
+
+		// Create the debug file
+		File debugFile = new File(System.getProperty("java.io.tmpdir"), Long.toHexString(cid) + ".txt");
+		try {
+			_debugWriter = new PrintWriter(new FileWriter(debugFile), false);
+		} catch (IOException ie) {
+			log.warn("Error creating debug file " + debugFile.getAbsolutePath());
+		}
+		
+		// Log connection info
+		InetAddress addr = sc.socket().getInetAddress();
+		_debugWriter.println("Connection to " + addr.getHostAddress() + "(" + addr.getHostName() + ")");
+		_debugWriter.println("Connected on " + new java.util.Date());
+		_debugWriter.println("*****");
+	}
+
+	/**
+	 * Closes the connection.
+	 */
+	public void close() {
+		super.close();
+		
+		// Log connection close
+		_debugWriter.println("*****");
+		_debugWriter.println("Closed on " + new java.util.Date());
+		_debugWriter.flush();
+		_debugWriter.close();
+	}
+
+	/**
+	 * Reads a message from the connection.
+	 * @return the XML message
+	 */
+	String read() throws SocketException, ProtocolException {
+		String msg = super.read();
+
+		// Dump the message
+		_debugWriter.println("-- in");
+		_debugWriter.println(msg);
+		_debugWriter.println();
+		_debugWriter.flush();
+		return msg;
+	}
+
+	/**
+	 * Writes a message to the connection
+	 * @param msg the message text
+	 */
+	public void write(String msg) {
+		super.write(msg);
+		if (msg != null) {
+			_debugWriter.println("-- out");
+			_debugWriter.println(msg);
+			_debugWriter.println();
+			_debugWriter.flush();	
+		}
+	}
+}
