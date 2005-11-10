554857359dd0f810a6c5d697521199f289b2a983
Luke Kolin <luke@deltava.org>
2005-11-10 21:50:44 +0000

    More efficient channel write code
    git-svn-id: file:///home/svn/acars2006@1654 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/beans/ACARSConnection.java
+++ src/java/org/deltava/acars/beans/ACARSConnection.java
@@ -23,320 +23,324 @@ import org.deltava.util.system.SystemData;
  */
 public class ACARSConnection implements Serializable, Comparable {
 
-   protected static final Logger log = Logger.getLogger(ACARSConnection.class);
+	protected static final Logger log = Logger.getLogger(ACARSConnection.class);
 
-   // Byte byffer decoder and character set
-   private final CharsetDecoder decoder = Charset.forName("ISO-8859-1").newDecoder();
+	// Byte byffer decoder and character set
+	private final CharsetDecoder decoder = Charset.forName("ISO-8859-1").newDecoder();
 
-   private SocketChannel _channel;
-   private InetAddress _remoteAddr;
-   private String _remoteHost;
-   private int protocolVersion = 1;
-   private int clientVersion;
+	private SocketChannel _channel;
+	private InetAddress _remoteAddr;
+	private String _remoteHost;
+	private int protocolVersion = 1;
+	private int clientVersion;
 
-   // Input/output network buffers
-   private ByteBuffer _iBuffer;
-   private ByteBuffer _oBuffer;
+	// Input/output network buffers
+	private ByteBuffer _iBuffer;
+	private ByteBuffer _oBuffer;
 
-   // The the actual buffer for messages
-   private StringBuffer _msgBuffer;
+	// The the actual buffer for messages
+	private StringBuffer _msgBuffer;
 
-   // Connection information
-   private long id;
-   private Pilot _userInfo;
-   private UserData _userData;
-   private InfoMessage _fInfo;
-   private PositionMessage _pInfo;
+	// Connection information
+	private long id;
+	private Pilot _userInfo;
+	private UserData _userData;
+	private InfoMessage _fInfo;
+	private PositionMessage _pInfo;
 
-   // Activity monitors
-   private long startTime;
-   private long lastActivityTime;
+	// Activity monitors
+	private long startTime;
+	private long lastActivityTime;
 
-   // Statistics
-   private long bytesIn;
-   private long bytesOut;
-   private long msgsIn;
-   private long msgsOut;
+	// Statistics
+	private long bytesIn;
+	private long bytesOut;
+	private long msgsIn;
+	private long msgsOut;
 
-   public ACARSConnection(long cid, SocketChannel sc) {
+	public ACARSConnection(long cid, SocketChannel sc) {
 
-      // Init the superclass and start time
-      super();
-      this.startTime = System.currentTimeMillis();
-      this.id = cid;
+		// Init the superclass and start time
+		super();
+		this.startTime = System.currentTimeMillis();
+		this.id = cid;
 
-      // Get IP Address information
-      _remoteAddr = sc.socket().getInetAddress();
+		// Get IP Address information
+		_remoteAddr = sc.socket().getInetAddress();
 
-      // Turn off blocking
-      try {
-         sc.configureBlocking(false);
-      } catch (IOException ie) {
-         // Log our error and shut the connection
-         log.error("Cannot set non-blocking I/O from " + _remoteAddr.getHostAddress(), ie);
-         try {
-            sc.close();
-         } catch (Exception e) {
-         }
-      } finally {
-         _channel = sc;
-      }
+		// Turn off blocking
+		try {
+			sc.configureBlocking(false);
+		} catch (IOException ie) {
+			// Log our error and shut the connection
+			log.error("Cannot set non-blocking I/O from " + _remoteAddr.getHostAddress(), ie);
+			try {
+				sc.close();
+			} catch (Exception e) {
+			}
+		} finally {
+			_channel = sc;
+		}
 
-      // Allocate the buffers and output stack for this channel
-      _msgBuffer = new StringBuffer();
-      _iBuffer = ByteBuffer.allocateDirect(SystemData.getInt("acars.buffer.nio"));
-      _oBuffer = ByteBuffer.allocateDirect(SystemData.getInt("acars.buffer.nio"));
-   }
+		// Allocate the buffers and output stack for this channel
+		_msgBuffer = new StringBuffer();
+		_iBuffer = ByteBuffer.allocate(SystemData.getInt("acars.buffer.nio"));
+		_oBuffer = ByteBuffer.allocate(SystemData.getInt("acars.buffer.nio"));
+	}
 
-   public void close() {
-      // Clear the buffers
-      _iBuffer = null;
-      _oBuffer = null;
+	public void close() {
+		// Clear the buffers
+		_iBuffer = null;
+		_oBuffer = null;
 
-      // Close the socket
-      try {
-         _channel.close();
-      } catch (Exception e) {
-      }
-   }
+		// Close the socket
+		try {
+			_channel.close();
+		} catch (Exception e) {
+		}
+	}
 
-   public boolean equals(SocketChannel ch) {
-      return _channel.equals(ch);
-   }
+	public boolean equals(SocketChannel ch) {
+		return _channel.equals(ch);
+	}
 
-   public boolean equals(Object o2) {
+	public boolean equals(Object o2) {
 
-      // Check to make sure we are the same type
-      if (!(o2 instanceof ACARSConnection))
-         return false;
+		// Check to make sure we are the same type
+		if (!(o2 instanceof ACARSConnection))
+			return false;
 
-      // Do the cast and compare the connections
-      ACARSConnection c2 = (ACARSConnection) o2;
-      return equals(c2.getID());
-   }
+		// Do the cast and compare the connections
+		ACARSConnection c2 = (ACARSConnection) o2;
+		return equals(c2.getID());
+	}
 
-   public boolean equals(long cid) {
-      return (this.id == cid);
-   }
+	public boolean equals(long cid) {
+		return (this.id == cid);
+	}
 
-   public boolean equals(String pid) {
-      return _userInfo.getPilotCode().equals(pid);
-   }
+	public boolean equals(String pid) {
+		return _userInfo.getPilotCode().equals(pid);
+	}
 
-   public long getBytesIn() {
-      return this.bytesIn;
-   }
+	public long getBytesIn() {
+		return this.bytesIn;
+	}
 
-   public long getBytesOut() {
-      return this.bytesOut;
-   }
+	public long getBytesOut() {
+		return this.bytesOut;
+	}
 
-   SocketChannel getChannel() {
-      return _channel;
-   }
+	SocketChannel getChannel() {
+		return _channel;
+	}
 
-   public Socket getSocket() {
-      return _channel.socket();
-   }
+	public Socket getSocket() {
+		return _channel.socket();
+	}
 
-   public int getFlightID() {
-      return (_fInfo == null) ? 0 : _fInfo.getFlightID();
-   }
+	public int getFlightID() {
+		return (_fInfo == null) ? 0 : _fInfo.getFlightID();
+	}
 
-   public long getID() {
-      return this.id;
-   }
+	public long getID() {
+		return this.id;
+	}
 
-   public InfoMessage getFlightInfo() {
-      return _fInfo;
-   }
+	public InfoMessage getFlightInfo() {
+		return _fInfo;
+	}
 
-   public PositionMessage getPosition() {
-      return _pInfo;
-   }
+	public PositionMessage getPosition() {
+		return _pInfo;
+	}
 
-   public long getLastActivity() {
-      return this.lastActivityTime;
-   }
+	public long getLastActivity() {
+		return this.lastActivityTime;
+	}
 
-   public long getMsgsIn() {
-      return this.msgsIn;
-   }
+	public long getMsgsIn() {
+		return this.msgsIn;
+	}
 
-   public long getMsgsOut() {
-      return this.msgsOut;
-   }
+	public long getMsgsOut() {
+		return this.msgsOut;
+	}
 
-   public int getProtocolVersion() {
-      return this.protocolVersion;
-   }
-   
-   public int getClientVersion() {
-      return this.clientVersion;
-   }
+	public int getProtocolVersion() {
+		return this.protocolVersion;
+	}
 
-   public long getStartTime() {
-      return this.startTime;
-   }
+	public int getClientVersion() {
+		return this.clientVersion;
+	}
 
-   public String getRemoteAddr() {
-      return _remoteAddr.getHostAddress();
-   }
+	public long getStartTime() {
+		return this.startTime;
+	}
 
-   public String getRemoteHost() {
-      return (_remoteHost == null) ? _remoteAddr.getHostName() : _remoteHost;
-   }
+	public String getRemoteAddr() {
+		return _remoteAddr.getHostAddress();
+	}
 
-   public Pilot getUser() {
-      return _userInfo;
-   }
+	public String getRemoteHost() {
+		return (_remoteHost == null) ? _remoteAddr.getHostName() : _remoteHost;
+	}
 
-   public UserData getUserData() {
-      return _userData;
-   }
+	public Pilot getUser() {
+		return _userInfo;
+	}
 
-   public String getUserID() {
-      return isAuthenticated() ? _userInfo.getPilotCode() : getRemoteAddr();
-   }
+	public UserData getUserData() {
+		return _userData;
+	}
 
-   public boolean isAuthenticated() {
-      return (_userInfo != null);
-   }
+	public String getUserID() {
+		return isAuthenticated() ? _userInfo.getPilotCode() : getRemoteAddr();
+	}
 
-   public boolean isConnected() {
-      return _channel.isConnected();
-   }
+	public boolean isAuthenticated() {
+		return (_userInfo != null);
+	}
 
-   public void setFlightInfo(InfoMessage msg) {
-	   _fInfo = msg;
-   }
-   
-   public void setPosition(PositionMessage msg) {
-	   _pInfo = msg;
-   }
+	public boolean isConnected() {
+		return _channel.isConnected();
+	}
 
-   public void setProtocolVersion(int pv) {
-      if ((pv > 0) && (pv <= Message.PROTOCOL_VERSION))
-         this.protocolVersion = pv;
-   }
-   
-   public void setClientVersion(int ver) {
-      if (ver > 50)
-         clientVersion = ver;
-   }
+	public void setFlightInfo(InfoMessage msg) {
+		_fInfo = msg;
+	}
 
-   public void setUser(Pilot p) {
-      _userInfo = p;
-   }
+	public void setPosition(PositionMessage msg) {
+		_pInfo = msg;
+	}
 
-   public void setUserLocation(UserData ud) {
-      _userData = ud;
-   }
-   
-   public int compareTo(Object o2) {
-	   ACARSConnection c2 = (ACARSConnection) o2;
-	   if (!isAuthenticated())
-		   return -1;
-	   else if (!c2.isAuthenticated())
-		   return 1;
-	   
-	   Pilot usr = c2.getUser();
-	   return new Integer(_userInfo.getPilotNumber()).compareTo(new Integer(usr.getPilotNumber()));
-   }
+	public void setProtocolVersion(int pv) {
+		if ((pv > 0) && (pv <= Message.PROTOCOL_VERSION))
+			this.protocolVersion = pv;
+	}
 
-   /* Here are the basic I/O methods, read and write */
-   String read() throws SocketException, ProtocolException {
+	public void setClientVersion(int ver) {
+		if (ver > 50)
+			clientVersion = ver;
+	}
 
-      // Clear the buffer
-      _iBuffer.clear();
+	public void setUser(Pilot p) {
+		_userInfo = p;
+	}
 
-      // Try and read from the channel until end of stream
-      try {
-         _channel.read(_iBuffer);
-      } catch (IOException ie) {
-         throw new SocketException("Error reading channel - " + ie.getMessage());
-      }
+	public void setUserLocation(UserData ud) {
+		_userData = ud;
+	}
 
-      // if we got nothing, return null
-      if (_iBuffer.position() == 0)
-         throw new ProtocolException("Connection Closed");
+	public int compareTo(Object o2) {
+		ACARSConnection c2 = (ACARSConnection) o2;
+		if (!isAuthenticated())
+			return -1;
+		else if (!c2.isAuthenticated())
+			return 1;
 
-      // Set the limit on the buffer and return to the start
-      _iBuffer.flip();
+		Pilot usr = c2.getUser();
+		return new Integer(_userInfo.getPilotNumber()).compareTo(new Integer(usr.getPilotNumber()));
+	}
 
-      // Update the counters
-      bytesIn += _iBuffer.limit();
-      msgsIn++;
-      lastActivityTime = System.currentTimeMillis();
+	/* Here are the basic I/O methods, read and write */
+	String read() throws SocketException, ProtocolException {
 
-      // Reset the decoder and decode into a char buffer
-      try {
-         _msgBuffer.append(decoder.decode(_iBuffer).toString());
-      } catch (CharacterCodingException cce) {
-      }
+		// Clear the buffer
+		_iBuffer.clear();
 
-      // Now, search the start of an XML message in the buffer; if there's no open discard the whole thing
-      int sPos = _msgBuffer.indexOf(ProtocolInfo.REQ_ELEMENT_OPEN);
-      if (sPos == -1) {
-         if (_msgBuffer.indexOf(ProtocolInfo.XML_HEADER) == -1) {
-            log.warn("Malformed message - " + _msgBuffer.toString());
-            _msgBuffer.setLength(0);
-         }
+		// Try and read from the channel until end of stream
+		try {
+			_channel.read(_iBuffer);
+		} catch (IOException ie) {
+			throw new SocketException("Error reading channel - " + ie.getMessage());
+		}
 
-         // Return nothing
-         return null;
-      }
+		// if we got nothing, return null
+		if (_iBuffer.position() == 0)
+			throw new ProtocolException("Connection Closed");
 
-      // Get the end of the message - if there's an end element build a message and return it
-      int ePos = _msgBuffer.indexOf(ProtocolInfo.REQ_ELEMENT_CLOSE, sPos);
-      if (ePos == -1)
-         return null;
+		// Set the limit on the buffer and return to the start
+		_iBuffer.flip();
 
-      ePos += ProtocolInfo.REQ_ELEMENT_CLOSE.length();
+		// Update the counters
+		bytesIn += _iBuffer.limit();
+		msgsIn++;
+		lastActivityTime = System.currentTimeMillis();
 
-      // Get the XML message out of the buffer
-      StringBuffer msgOut = new StringBuffer(ProtocolInfo.XML_HEADER);
-      msgOut.append(_msgBuffer.substring(sPos, ePos));
+		// Reset the decoder and decode into a char buffer
+		try {
+			_msgBuffer.append(decoder.decode(_iBuffer).toString());
+		} catch (CharacterCodingException cce) {
+		}
 
-      // Clear the message out of the buffer
-      _msgBuffer.delete(0, ePos);
-      
-      // Return the buffer
-      return msgOut.toString();
-   }
+		// Now, search the start of an XML message in the buffer; if there's no open discard the whole thing
+		int sPos = _msgBuffer.indexOf(ProtocolInfo.REQ_ELEMENT_OPEN);
+		if (sPos == -1) {
+			if (_msgBuffer.indexOf(ProtocolInfo.XML_HEADER) == -1) {
+				log.warn("Malformed message - " + _msgBuffer.toString());
+				_msgBuffer.setLength(0);
+			}
 
-   public void write(String msg) {
-      if (msg == null)
-         return;
-      
-      try {
-         // Keep writing until the message is done
-         while (msg.length() > 0) {
-            _oBuffer.clear();
+			// Return nothing
+			return null;
+		}
 
-            // If the message is still larger than the buffer, then dump it out
-            if (msg.length() > _oBuffer.capacity()) {
-               String submsg = msg.substring(0, _oBuffer.capacity());
-               msg = msg.substring(_oBuffer.capacity());
-               _oBuffer.put(submsg.getBytes());
-               bytesOut += msg.length();
-            } else {
-               _oBuffer.put(msg.getBytes());
-               bytesOut += msg.length();
-               msg = "";
-            }
+		// Get the end of the message - if there's an end element build a message and return it
+		int ePos = _msgBuffer.indexOf(ProtocolInfo.REQ_ELEMENT_CLOSE, sPos);
+		if (ePos == -1)
+			return null;
 
-            // Flip the buffer and dump it all out
-            _oBuffer.flip();
-            while (_oBuffer.remaining() > 0)
-               _channel.write(_oBuffer);
-         }
-         
-         // Update statistics
-         msgsOut++;
-         lastActivityTime = System.currentTimeMillis();
-      } catch (IOException ie) {
-         log.error("Error writing to socket " + _remoteAddr.getHostAddress() + " - " + ie.getMessage(), ie);
-      }
-   }
+		ePos += ProtocolInfo.REQ_ELEMENT_CLOSE.length();
+
+		// Get the XML message out of the buffer
+		StringBuffer msgOut = new StringBuffer(ProtocolInfo.XML_HEADER);
+		msgOut.append(_msgBuffer.substring(sPos, ePos));
+
+		// Clear the message out of the buffer
+		_msgBuffer.delete(0, ePos);
+
+		// Return the buffer
+		return msgOut.toString();
+	}
+
+	public void write(String msg) {
+		if (msg == null)
+			return;
+
+		// FIXME Remove after testing
+		StringBuffer buf = new StringBuffer();
+		
+		int ofs = 0;
+		byte[] msgBytes = msg.getBytes();
+		try {
+			// Keep writing until the message is done
+			while (ofs <= msgBytes.length) {
+				_oBuffer.clear();
+
+				// Keep writing to the buffer
+				while ((ofs <= msgBytes.length) && (_oBuffer.remaining() > 0)) {
+					_oBuffer.put(msgBytes[ofs]);
+					buf.append(msgBytes[ofs]);
+					ofs++;
+				}
+
+				// Flip the buffer
+				_oBuffer.flip();
+				while (_oBuffer.remaining() > 0)
+					_channel.write(_oBuffer);
+			}
+			
+			// FIXME Remove after testing
+			assert msg.equals(buf.toString());
+			
+			// Update statistics
+			msgsOut++;
+			bytesOut += ofs;
+			lastActivityTime = System.currentTimeMillis();
+		} catch (Exception e) {
+			log.error("Error writing to socket " + _remoteAddr.getHostAddress() + " - " + e.getMessage(), e);
+		}
+	}
 }
