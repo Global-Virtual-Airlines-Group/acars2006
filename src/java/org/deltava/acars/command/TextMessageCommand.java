91b90dc82a5b741edabb907d847835171f680a8b
Luke Kolin <luke@deltava.org>
2006-02-14 20:08:00 +0000

    Add profanity filter
    git-svn-id: file:///home/svn/acars2006@2291 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/TextMessageCommand.java
+++ src/java/org/deltava/acars/command/TextMessageCommand.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
+// Copyright (c) 2004, 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.command;
 
 import java.util.*;
@@ -12,9 +12,11 @@ import org.deltava.acars.message.*;
 
 import org.deltava.acars.util.TextMessageCache;
 
+import org.deltava.util.*;
 import org.deltava.util.system.SystemData;
 
 /**
+ * An ACARS server command to send text messages.
  * @author Luke
  * @version 1.0
  * @since 1.0
@@ -34,6 +36,23 @@ public class TextMessageCommand extends ACARSCommand {
 		// Get the inbound message and the owner
 		Pilot usr = env.getOwner();
 		TextMessage msg = (TextMessage) env.getMessage();
+		if (usr == null) {
+			log.warn("Attempt to send anonymous message");
+			return;
+		}
+			
+		// If we have messaging restrictions on this user, apply the profanity filter
+		switch (usr.getACARSRestriction()) {
+			case Pilot.ACARS_NOMSGS:
+				log.warn(usr.getName() + " attempted to send message!");
+				return;
+				
+			case Pilot.ACARS_RESTRICT:
+				if (ProfanityFilter.flag(msg.getText())) {
+					log.warn("Questionable content received from " + usr.getName());
+					msg.setText(ProfanityFilter.filter(msg.getText()));
+				}
+		}
 		
 		// Create the outbound message
 		TextMessage txtRsp = new TextMessage(usr, msg.getText());
@@ -51,9 +70,9 @@ public class TextMessageCommand extends ACARSCommand {
 			ctx.pushAll(txtRsp, env.getConnectionID());
 			log.info("Public message from " + usr.getPilotCode());
 		} else {
-			Collection dstC = ctx.getACARSConnections(msg.getRecipient());
-			for (Iterator i = dstC.iterator(); i.hasNext(); ) {
-				ACARSConnection ac = (ACARSConnection) i.next();
+			Collection<ACARSConnection> dstC = ctx.getACARSConnections(msg.getRecipient());
+			for (Iterator<ACARSConnection> i = dstC.iterator(); i.hasNext(); ) {
+				ACARSConnection ac = i.next();
 				log.info("Message from " + usr.getPilotCode() + " to " + ac.getUserID());
 				rUsr = ac.getUser();
 				ctx.push(txtRsp, ac.getID());
