5395e28fc1cf29d003b7b7a0efe59b26f6606051
Luke Kolin <luke@deltava.org>
2008-04-21 05:27:12 +0000

    Send AUTH requests when unauthenticated
    git-svn-id: file:///home/svn/acars2006@5085 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/message/AcknowledgeMessage.java
+++ src/java/org/deltava/acars/message/AcknowledgeMessage.java
@@ -1,4 +1,4 @@
-// Copyright 2004, 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2004, 2005, 2006, 2008 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.message;
 
 import java.util.*;
@@ -8,32 +8,55 @@ import org.deltava.beans.Pilot;
 /**
  * An ACARS Acknowledgement message.
  * @author Luke
- * @version 1.0
+ * @version 2.1
  * @since 1.0
  */
 
 public class AcknowledgeMessage extends AbstractMessage {
 
 	private long _parent;
-	private Map<String, String> _msgs = new LinkedHashMap<String, String>();
+	private final Map<String, String> _msgs = new LinkedHashMap<String, String>();
 
+	/**
+	 * Creates the message.
+	 * @param msgFrom the originating pilot, either the author or the system
+	 * @param parentID the parent message ID
+	 */
 	public AcknowledgeMessage(Pilot msgFrom, long parentID) {
 		super(Message.MSG_ACK, msgFrom);
 		_parent = parentID;
 	}
 	
+	/**
+	 * Returns the parent message ID.
+	 * @return the message ID
+	 */
 	public long getParentID() {
 		return _parent;
 	}
 	
+	/**
+	 * Retrieves a message attribute.
+	 * @param eName the attribute name
+	 * @return the attribute value, or null if not found
+	 */
 	public String getEntry(String eName) {
 		return _msgs.get(eName);
 	}
 	
+	/**
+	 * Returns the names of all message attributes.
+	 * @return a Collection of attribute names
+	 */
 	public Collection<String> getEntryNames() {
 		return _msgs.keySet();
 	}
 	
+	/**
+	 * Adds a message attribute. If the attribute already exists, it will be overwritten.
+	 * @param eName the attribute name
+	 * @param eValue the attribute value
+	 */
 	public void setEntry(String eName, String eValue) {
 		_msgs.put(eName, eValue);
 	}
