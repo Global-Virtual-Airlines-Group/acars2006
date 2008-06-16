df60cc7a9d0081ff282baca365b4e300d5115cdd
Luke Kolin <luke@deltava.org>
2008-06-16 01:47:45 +0000

    Add oceanic track support
    git-svn-id: file:///home/svn/acars2006@5195 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/acars/command/data/OceanicTrackCommand.java
@@ -0,0 +1,60 @@
+// Copyright 2008 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.acars.command.data;
+
+import java.util.*;
+
+import org.deltava.beans.schedule.*;
+import org.deltava.dao.*;
+import org.deltava.util.*;
+
+import org.deltava.acars.beans.MessageEnvelope;
+
+import org.deltava.acars.command.*;
+import org.deltava.acars.message.*;
+import org.deltava.acars.message.data.OceanicTrackMessage;
+
+/**
+ * An ACARS data command to return available NAT data.
+ * @author Luke
+ * @version 2.2
+ * @since 2.2
+ */
+
+public class OceanicTrackCommand extends DataCommand {
+
+	/**
+	 * Initializes the Command.
+	 */
+	public OceanicTrackCommand() {
+		super(OceanicTrackCommand.class);
+	}
+
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @param env the message Envelope
+	 */
+	public void execute(CommandContext ctx, MessageEnvelope env) {
+		
+		// Get the message
+		DataRequestMessage msg = (DataRequestMessage) env.getMessage();
+		OceanicTrackMessage rspMsg = new OceanicTrackMessage(env.getOwner(), msg.getID());
+		
+		// Get the date
+		Date dt = msg.hasFlag("date") ? StringUtils.parseDate(msg.getFlag("date"), "MM/dd/yyyy") : null;
+		try {
+			GetRoute dao = new GetRoute(ctx.getConnection());
+			rspMsg.addAll(dao.getOceanicTrakcs(OceanicRoute.NAT, dt).values());
+		} catch (DAOException de) {
+			log.error("Error loading NAT tracks for " + msg.getFlag("date") + " - " + de.getMessage(), de);
+			AcknowledgeMessage errMsg = new AcknowledgeMessage(env.getOwner(), msg.getID());
+			errMsg.setEntry("error", "Cannot load " + msg.getFlag("date") + " NAT data");
+			ctx.push(errMsg, ctx.getACARSConnection().getID());
+		} finally {
+			ctx.release();
+		}
+		
+		// Push the message
+		ctx.push(rspMsg, env.getConnectionID());
+	}
+}
