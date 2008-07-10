53ce71e66970d4985c471395a5701f453d52df0f
Luke Kolin <luke@deltava.org>
2008-07-10 16:41:47 +0000

    Allow hiding while connected
    git-svn-id: file:///home/svn/acars2006@5275 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/acars/command/data/HiddenCommand.java
@@ -0,0 +1,66 @@
+// Copyright 2008 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.acars.command.data;
+
+import java.util.Iterator;
+
+import org.deltava.acars.beans.*;
+import org.deltava.acars.command.*;
+import org.deltava.acars.message.*;
+import org.deltava.acars.message.data.ConnectionMessage;
+
+/**
+ * An ACARS command to toggle a Pilot's hidden status.
+ * @author Luke
+ * @version 2.2
+ * @since 2.2
+ */
+
+public class HiddenCommand extends DataCommand {
+
+	/**
+	 * Initializes the command.
+	 */
+	public HiddenCommand() {
+		super(HiddenCommand.class);
+	}
+
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @param env the message Envelope
+	 */
+	public final void execute(CommandContext ctx, MessageEnvelope env) {
+		
+		// Get the message and connection
+		DataRequestMessage msg = (DataRequestMessage) env.getMessage();
+		ACARSConnection ac = ctx.getACARSConnection();
+		if (!ac.getUser().isInRole("HR")) {
+			log.warn(ac.getUserID() + " attempting to hide connection!");
+			return;	
+		}
+		
+		// Set the pilot's hidden flag
+		boolean isNowHidden = Boolean.valueOf(msg.getFlag("isHidden")).booleanValue(); 
+		ac.setUserHidden(isNowHidden);
+		
+		// Send the update message
+		int msgType = isNowHidden ? DataMessage.REQ_REMOVEUSER : DataMessage.REQ_ADDUSER;
+		ConnectionMessage dmsg = new ConnectionMessage(env.getOwner(), msgType, ac.getID());
+		dmsg.add(ac);
+		
+		// Push the response
+		for (Iterator<ACARSConnection> i = ctx.getACARSConnections("*").iterator(); i.hasNext(); ) {
+			ACARSConnection con = i.next();
+			if ((con.getID() != ac.getID()) && !con.getUser().isInRole("HR"))
+				ctx.push(dmsg, con.getID());
+		}
+	}
+	
+	/**
+	 * Returns the maximum execution time of this command before a warning is issued.
+	 * @return the maximum execution time in milliseconds
+	 */
+	public final int getMaxExecTime() {
+		return 225;
+	}
+}
--- src/java/org/deltava/acars/workers/LogicProcessor.java
+++ src/java/org/deltava/acars/workers/LogicProcessor.java
@@ -2,7 +2,7 @@
 package org.deltava.acars.workers;
 
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.sql.Connection;
 
 import org.deltava.acars.beans.*;
@@ -27,6 +27,7 @@ import org.deltava.util.system.SystemData;
 public class LogicProcessor extends Worker {
 	
 	private QueueingThreadPool _cmdPool;
+	private static final Semaphore _dbLock = new Semaphore(1, true);
 	
 	private final Map<Integer, ACARSCommand> _commands = new HashMap<Integer, ACARSCommand>();
 	private final Map<Integer, DataCommand> _dataCommands = new HashMap<Integer, DataCommand>();
@@ -48,7 +49,7 @@ public class LogicProcessor extends Worker {
 		int maxThreads = SystemData.getInt("acars.pool.threads.logic.max", minThreads);
 		_cmdPool = new QueueingThreadPool(minThreads, maxThreads, 2500, LogicProcessor.class);
 		_cmdPool.allowCoreThreadTimeOut(false);
-		_cmdPool.setSortBase(40);
+		_cmdPool.setSortBase(_status.getSortOrder());
 
 		// Initialize commands
 		_commands.put(Integer.valueOf(Message.MSG_ACK), new DummyCommand());
@@ -62,9 +63,12 @@ public class LogicProcessor extends Worker {
 		_commands.put(Integer.valueOf(Message.MSG_PIREP), new FilePIREPCommand());
 		_commands.put(Integer.valueOf(Message.MSG_ERROR), new ErrorCommand());
 		_commands.put(Integer.valueOf(Message.MSG_DIAG), new DiagnosticCommand());
+		_commands.put(Integer.valueOf(Message.MSG_MPUPDATE), new MPInfoCommand());
+		_commands.put(Integer.valueOf(Message.MSG_MPINIT), new MPInitCommand());
 
 		// Initialize data commands
 		_dataCommands.put(Integer.valueOf(DataMessage.REQ_BUSY), new BusyCommand());
+		_dataCommands.put(Integer.valueOf(DataMessage.REQ_HIDE), new HiddenCommand());
 		_dataCommands.put(Integer.valueOf(DataMessage.REQ_CHARTS), new ChartsCommand());
 		_dataCommands.put(Integer.valueOf(DataMessage.REQ_DRAFTPIREP), new DraftFlightCommand());
 		_dataCommands.put(Integer.valueOf(DataMessage.REQ_USRLIST), new UserListCommand());
@@ -218,44 +222,43 @@ public class LogicProcessor extends Worker {
 				
 				// Log the received message and get the command to process it
 				Message msg = env.getMessage();
-				log.debug(Message.MSG_TYPES[msg.getType()] + " message from " + env.getOwnerID());
+				if (log.isDebugEnabled())
+					log.debug(Message.MSG_TYPES[msg.getType()] + " message from " + env.getOwnerID());
+				
 				ACARSCommand cmd = null;
 				if (msg.getType() == Message.MSG_DATAREQ) {
 					DataRequestMessage reqmsg = (DataRequestMessage) msg;
 					cmd = _dataCommands.get(Integer.valueOf(reqmsg.getRequestType()));
 					String reqType = DataMessage.REQ_TYPES[reqmsg.getRequestType()];
-					if (cmd == null) {
+					if (cmd == null)
 						log.warn("No Data Command for " + reqType + " request");
-						return;
-					}
-
-					// Log invocation
-					log.info("Data Request (" + reqType + ") from " + env.getOwnerID());
+					else
+						log.info("Data Request (" + reqType + ") from " + env.getOwnerID());
 				} else if (msg.getType() == Message.MSG_DISPATCH) {
 					DispatchMessage dspmsg = (DispatchMessage) msg;
 					cmd = _dspCommands.get(Integer.valueOf(dspmsg.getRequestType()));
 					String reqType = DispatchMessage.REQ_TYPES[dspmsg.getRequestType()];
-					if (cmd == null) {
+					if (cmd == null)
 						log.warn("No Dispatch Command for " + reqType + " request");
-						return;
-					}
-					
-					// Log invocation
-					log.info("Dispatch Request (" + reqType + ") from " + env.getOwnerID());
+					else
+						log.info("Dispatch Request (" + reqType + ") from " + env.getOwnerID());
 				} else {
 					cmd = _commands.get(Integer.valueOf(msg.getType()));
-					if (cmd == null) {
+					if (cmd == null)
 						log.warn("No command for " + Message.MSG_TYPES[msg.getType()] + " message");
-						return;
-					}
 				}
 				
 				// Send the envelope to the thread pool for processing
-				_cmdPool.execute(new CommandWorker(env, cmd));
+				if (cmd != null)
+					_cmdPool.execute(new CommandWorker(env, cmd));
 				
 				// Flush the logs
-				if (SetStatistics.getMaxAge() > 30000) 
-					flushLogs();
+				if (_dbLock.tryAcquire()) {
+					if (SetStatistics.getMaxAge() > 30000) 
+						flushLogs();
+				
+					_dbLock.release();
+				}
 				
 				_status.complete();
 			} catch (InterruptedException ie) {
