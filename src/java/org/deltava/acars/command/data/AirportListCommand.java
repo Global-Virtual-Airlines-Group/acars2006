6e745a419d5772ed202c981c2edd99b160eaf324
Luke Kolin <luke@deltava.org>
2008-11-04 23:55:57 +0000

    Load airport/airlines for user's specific airline
    git-svn-id: file:///home/svn/acars2006@5540 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/data/AirportListCommand.java
+++ src/java/org/deltava/acars/command/data/AirportListCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2008 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.command.data;
 
 import java.util.*;
@@ -12,12 +12,14 @@ import org.deltava.acars.message.data.AirportMessage;
 import org.deltava.beans.schedule.Airport;
 import org.deltava.comparators.AirportComparator;
 
+import org.deltava.dao.*;
+
 import org.deltava.util.system.SystemData;
 
 /**
  * An ACARS data command to return available Airport data.
  * @author Luke
- * @version 1.0
+ * @version 2.3
  * @since 1.0
  */
 
@@ -41,14 +43,23 @@ public class AirportListCommand extends DataCommand {
 		// Get the message
 		DataRequestMessage msg = (DataRequestMessage) env.getMessage();
 		AirportMessage rspMsg = new AirportMessage(env.getOwner(), msg.getID());
-
-		// Get all airports
-		Map allAirports = (Map) SystemData.getObject("airports");
-		Set<Airport> airports = new TreeSet<Airport>(new AirportComparator(AirportComparator.NAME));
-		airports.addAll(allAirports.values());
-		rspMsg.addAll(airports);
 		
+		// Load the airports for the user
+		Collection<Airport> airports = new TreeSet<Airport>(new AirportComparator(AirportComparator.NAME));
+		try {
+			GetAirport dao = new GetAirport(ctx.getConnection());
+			dao.setAppCode(ctx.getACARSConnection().getUserData().getAirlineCode());
+			airports.addAll(dao.getAll().values());
+		} catch (DAOException de) {
+			log.error("Cannot load airports - " + de.getMessage(), de);
+			Map allAirports = (Map) SystemData.getObject("airports");
+			airports.addAll(allAirports.values());
+		} finally {
+			ctx.release();
+		}
+
 		// Push the response
+		rspMsg.addAll(airports);
 		ctx.push(rspMsg, env.getConnectionID());
 	}
 }
