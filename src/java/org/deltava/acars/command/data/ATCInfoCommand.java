5d9f890627f75e3fe57e41a6474d9c639f8b9068
Luke Kolin <luke@deltava.org>
2009-01-12 15:29:29 +0000

    Load ServInfo data from local fileystem only
    git-svn-id: file:///home/svn/acars2006@5727 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/data/ATCInfoCommand.java
+++ src/java/org/deltava/acars/command/data/ATCInfoCommand.java
@@ -1,6 +1,7 @@
 // Copyright 2005, 2006, 2008, 2009 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.command.data;
 
+import java.io.*;
 import java.util.Collection;
 
 import org.deltava.acars.beans.MessageEnvelope;
@@ -11,16 +12,15 @@ import org.deltava.acars.message.data.ControllerMessage;
 
 import org.deltava.beans.OnlineNetwork;
 import org.deltava.beans.servinfo.NetworkInfo;
+
 import org.deltava.dao.file.GetServInfo;
 
-import org.deltava.util.ThreadUtils;
 import org.deltava.util.system.SystemData;
-import org.deltava.util.servinfo.ServInfoLoader;
 
 /**
  * An ACARS Server command to display online ATC data.
  * @author Luke
- * @version 2.3
+ * @version 2.4
  * @since 1.0
  */
 
@@ -52,48 +52,17 @@ public class ATCInfoCommand extends DataCommand {
 			log.warn("Unknown Online network - " + msg.getFlag("network"));
 			return;
 		}
-
-		// Get the network info from the cache
-		NetworkInfo info = GetServInfo.getCachedInfo(network);
-		ServInfoLoader loader = new ServInfoLoader(network);
 		
-		// If we get null, then block until we can load it; if we're expired, spawn a new loader thread
-		if ((info == null) && (!ServInfoLoader.isLoading(network))) {
-			log.warn("Loading " + network + " data in main thread");
-			Thread t = null;
-			synchronized (ServInfoLoader.class) {
-				t = new Thread(loader, network + " ServInfo Loader");
-				t.setDaemon(true);
-				t.setPriority(Math.max(Thread.MIN_PRIORITY, Thread.currentThread().getPriority() - 1));
-				ServInfoLoader.addLoader(network, t);
+		// Get the data
+		NetworkInfo info = null;
+		try {
+			File f = new File(SystemData.get("online." + network.toString().toLowerCase() + ".local.info"));
+			if (f.exists()) {
+				GetServInfo sidao = new GetServInfo(new FileInputStream(f));
+				info = sidao.getInfo(network);
 			}
-
-			// Wait for the thread to exit
-			int totalTime = 0;
-			while (ThreadUtils.isAlive(t) && (totalTime < 10000)) {
-				totalTime += 250;
-				ThreadUtils.sleep(250);
-			}
-
-			// If the thread hasn't died, then kill it
-			if (totalTime >= 10000) {
-				ThreadUtils.kill(t, 1000);
-				info = new NetworkInfo(network);
-			} else
-				info = loader.getInfo();
-		} else if (info == null)
-			info = new NetworkInfo(network);
-		else if (info.getExpired()) {
-			synchronized (ServInfoLoader.class) {
-				if (!ServInfoLoader.isLoading(network)) {
-					log.info("Spawning new ServInfo load thread for " + network);
-					Thread t = new Thread(loader, network + " ServInfo Loader");
-					t.setDaemon(true);
-					t.setPriority(Math.max(Thread.MIN_PRIORITY, Thread.currentThread().getPriority() - 1));
-					ServInfoLoader.addLoader(network, t);
-				} else
-					log.warn("Already loading " + network + " information");
-			}
+		} catch (Exception e) {
+			log.error("Cannot load " + network + " ServInfo feed - " + e.getMessage(), e);
 		}
 
 		// Filter the controllers based on range from position
