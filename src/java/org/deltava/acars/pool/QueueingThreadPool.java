c12ef6e8d76b4a6176d425714f38f836b3a48173
Luke Kolin <luke@deltava.org>
2009-07-20 14:47:48 +0000

    Add additional logging when queue backs up
    git-svn-id: file:///home/svn/acars2006@6045 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/pool/QueueingThreadPool.java
+++ src/java/org/deltava/acars/pool/QueueingThreadPool.java
@@ -1,4 +1,4 @@
-// Copyright 2007, 2008 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2008, 2009 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.pool;
 
 import java.util.*;
@@ -12,7 +12,7 @@ import org.deltava.acars.beans.*;
  * A Thread Pool executor that implements built-in queueing. This allows the thread pool to
  * continue to take work units even if the dynamic thread pool reaches its maximum size. 
  * @author Luke
- * @version 2.1
+ * @version 2.6
  * @since 2.0
  */
 
@@ -22,7 +22,7 @@ public class QueueingThreadPool extends ThreadPoolExecutor implements PoolWorker
 	private PoolWorkerFactory _tFactory;
 	private int _sortOrderBase;
 	
-	private final Map<Integer, LatencyWorkerStatus> _status = new ConcurrentHashMap<Integer, LatencyWorkerStatus>();
+	protected final Map<Integer, LatencyWorkerStatus> _status = new ConcurrentHashMap<Integer, LatencyWorkerStatus>();
 	protected final BlockingQueue<PoolWorker> _queuedEntries = new LinkedBlockingQueue<PoolWorker>();
 
 	/**
@@ -30,6 +30,7 @@ public class QueueingThreadPool extends ThreadPoolExecutor implements PoolWorker
 	 */
 	class QueueHandler implements RejectedExecutionHandler {
 		private long _lastEntryTime;
+		private boolean _queueBackup;
 		
 		public void rejectedExecution(Runnable r, ThreadPoolExecutor pool) {
 			if ((r instanceof PoolWorker) && (!pool.isTerminating())) {
@@ -37,9 +38,19 @@ public class QueueingThreadPool extends ThreadPoolExecutor implements PoolWorker
 				_queuedEntries.add((PoolWorker) r);
 
 				// Check if we log
-				int size = _queuedEntries.size(); 
-				if (((now - _lastEntryTime) > 2500) && (size > 3)) 
+				int size = _queuedEntries.size();
+				boolean queueBackup = (size > 40);
+				if (!_queueBackup && queueBackup) {
+					_queueBackup = true;
+					log.error("Queue appears backed up, size =" + size);
+					for (Iterator<Map.Entry<Integer, LatencyWorkerStatus>> i = _status.entrySet().iterator(); i.hasNext(); ) {
+						Map.Entry<Integer, ? extends WorkerStatus> e = i.next();
+						log.error("Worker " + e.getKey() + " = " + e.getValue().getMessage());
+					}
+				} else if (((now - _lastEntryTime) > 2500) && (size > 3)) 
 					log.warn("Thread pool full - queueing entry #" + size);
+				else
+					_queueBackup &= queueBackup;
 				
 				_lastEntryTime = now;
 			}
@@ -53,8 +64,8 @@ public class QueueingThreadPool extends ThreadPoolExecutor implements PoolWorker
 	 * @param keepAliveTime each thread's idle keepalive time in milliseconds
 	 * @param logClass the Logging class name
 	 */
-	public QueueingThreadPool(int coreSize, int maxSize, long keepAliveTime, Class logClass) {
-		super(coreSize, Math.max(coreSize, maxSize), keepAliveTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(4));
+	public QueueingThreadPool(int coreSize, int maxSize, long keepAliveTime, Class<?> logClass) {
+		super(coreSize, Math.max(coreSize, maxSize), keepAliveTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5));
 		log = Logger.getLogger(logClass);
 		_tFactory = new PoolWorkerFactory(logClass.getSimpleName());
 		setThreadFactory(_tFactory);
@@ -70,11 +81,19 @@ public class QueueingThreadPool extends ThreadPoolExecutor implements PoolWorker
 	}
 	
 	/**
-	 * Returns the status counters for each worker  bucket.
+	 * Returns the status counters for each worker bucket.
 	 * @return a Collection of WorkerStatus beans
 	 */
 	public Collection<WorkerStatus> getWorkerStatus() {
 		return new TreeSet<WorkerStatus>(_status.values());
+	}
+	
+	/**
+	 * Returns the number of tasks queued to be executed.
+	 * @return the number of tasks
+	 */
+	public int getTasksWaiting() {
+		return _queuedEntries.size();
 	}
 	
 	/**
