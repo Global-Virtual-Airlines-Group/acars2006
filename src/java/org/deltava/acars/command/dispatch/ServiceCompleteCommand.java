bb88b916b7f4a6f70be0c32cc0a588533f7731a0
Luke Kolin <luke@deltava.org>
2009-12-23 20:05:48 +0000

    Send acceptance to recipient
    git-svn-id: file:///home/svn/acars2006@6387 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/dispatch/ServiceCompleteCommand.java
+++ src/java/org/deltava/acars/command/dispatch/ServiceCompleteCommand.java
@@ -1,15 +1,16 @@
-// Copyright 2007, 2008 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2008, 2009 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.command.dispatch;
 
 import org.deltava.acars.beans.*;
 import org.deltava.acars.command.*;
 
 import org.deltava.acars.message.dispatch.CompleteMessage;
+import org.deltava.util.StringUtils;
 
 /**
  * An ACARS Command to handle Dispatch service completion notifications.
  * @author Luke
- * @version 2.1
+ * @version 2.7
  * @since 2.0
  */
 
@@ -35,7 +36,27 @@ public class ServiceCompleteCommand extends DispatchCommand {
 		// Cancel this connection's dispatch status
 		ACARSConnection ac = ctx.getACARSConnection();
 		long dspID = ac.getDispatcherID();
+		ACARSConnection dc = ctx.getACARSConnectionPool().get(Long.valueOf(dspID));
 		ac.setDispatcherID(0);
+		
+		// Find the dispatcher to send to
+		String dispatcherID = msg.getRecipient();
+		if (!StringUtils.isEmpty(dispatcherID)) {
+			ACARSConnection dc2 = ctx.getACARSConnection(dispatcherID);
+			if ((dc2 != null) && (dc2.getID() != dspID)) {
+				dspID = dc2.getID();
+				if (dc != null)
+					log.warn("Sending Accept to " + dc2.getUserID() + ", expected " + dc.getUserID());
+				else
+					log.warn("Sending Accept to " + dc2.getUserID());
+			} else if (dc2 == null)
+				log.warn("Cannot send Accept to " + dispatcherID);
+			else
+				log.warn("Sending Accept to " + dc2.getUserID());
+		} else if (dc == null)
+			log.warn("Unknown Dispatch connection - " + dspID);
+		else
+			log.warn("Sending Accept to " + dc.getUserID());
 
 		// Send to the dispatcher
 		ctx.push(msg, dspID);
