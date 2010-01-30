36d812da131413cbb77c1d95b8b2ae729f67c172
Luke Kolin <luke@deltava.org>
2010-01-30 22:02:38 +0000

    Submit dispatcher ID in PIREP
    git-svn-id: file:///home/svn/acars2006@6457 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/message/FlightReportMessage.java
+++ src/java/org/deltava/acars/message/FlightReportMessage.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2009 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.message;
 
 import org.deltava.beans.Pilot;
@@ -7,13 +7,15 @@ import org.deltava.beans.flight.ACARSFlightReport;
 /**
  * An ACARS Message used to pass PIREP data.
  * @author Luke
- * @version 2.7
+ * @version 2.8
  * @since 1.0
  */
 
 public class FlightReportMessage extends AbstractMessage {
    
    private ACARSFlightReport _afr;
+   private int _dispatcherID;
+   private int _routeID;
 
    /**
     * Creates a new Flight Report message.
@@ -21,6 +23,38 @@ public class FlightReportMessage extends AbstractMessage {
     */
    public FlightReportMessage(Pilot msgFrom) {
       super(MSG_PIREP, msgFrom);
+   }
+   
+   /**
+    * Returns the PIREP data.
+    * @return the ACARSFlightReport bean
+    */
+   public ACARSFlightReport getPIREP() {
+      return _afr;
+   }
+   
+   /**
+    * Returns whether this flight used a Dispatch-generated flight plan.
+    * @return TRUE if Dispatch used, otherwise FALSE
+    */
+   public boolean isDispatch() {
+	   return (_routeID != 0);
+   }
+   
+   /**
+    * Returns the ID of the Dispatcher used to plot this flight.
+    * @return the Dispatcher's database ID, or zero for auto-dispatch
+    */
+   public int getDispatcherID() {
+	   return _dispatcherID;
+   }
+   
+   /**
+    * Returns the ID of the Route used on this flight.
+    * @return the Route's database ID
+    */
+   public int getRouteID() {
+	   return _routeID;
    }
 
    /**
@@ -30,12 +64,20 @@ public class FlightReportMessage extends AbstractMessage {
    public void setPIREP(ACARSFlightReport afr) {
       _afr = afr;
    }
+  
+   /**
+    * Sets the ID of the Dispatcher used to plot this flight.
+    * @param id the Dispatcher's database ID, or zero for auto-dispatch
+    */
+   public void setDispatcherID(int id) {
+	   _dispatcherID = id;
+   }
    
    /**
-    * Returns the PIREP data.
-    * @return the ACARSFlightReport bean
+    * Sets the ID of the Route used on this flight.
+    * @param id the Route's database ID
     */
-   public ACARSFlightReport getPIREP() {
-      return _afr;
+   public void setRouteID(int id) {
+	   _routeID = id;
    }
 }
