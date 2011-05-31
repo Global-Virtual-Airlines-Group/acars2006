f88ee4cab89ab126f8038d9cdb86dc8509cdaf1e
Luke Kolin <luke@deltava.org>
2011-05-31 05:05:18 +0000

    Cleanup
    git-svn-id: file:///home/svn/acars2006@7499 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/message/ErrorMessage.java
+++ src/java/org/deltava/acars/message/ErrorMessage.java
@@ -1,3 +1,4 @@
+// Copyright 2004, 2011 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.message;
 
 import org.deltava.beans.Pilot;
@@ -5,29 +6,31 @@ import org.deltava.beans.Pilot;
 /**
  * Error message. This is an internally generated message and not part of the ACARS wire protocol.
  * @author Luke
- * @version 1.0
+ * @version 4.0
  * @since 1.0
  */
 
 public class ErrorMessage extends AbstractMessage {
 
 	private String _errMsg;
-	
-	public ErrorMessage(Pilot msgTo, String msg) {
+
+	/**
+	 * Creates a new error message.
+	 * @param msgTo the Pilot to send to 
+	 * @param msg the error message
+	 * @param id the message ID
+	 */
+	public ErrorMessage(Pilot msgTo, String msg, long id) {
 		super(Message.MSG_ERROR, msgTo);
 		_errMsg = msg;
-	}
-	
-	public ErrorMessage(Pilot msgTo, String msg, long id) {
-		this(msgTo, msg);
 		setID(id);
 	}
-	
+
+	/**
+	 * Returns the error message text.
+	 * @return the message text
+	 */
 	public String getText() {
 		return _errMsg;
-	}
-	
-	public void setText(String msg) {
-		_errMsg = msg;
 	}
 }
