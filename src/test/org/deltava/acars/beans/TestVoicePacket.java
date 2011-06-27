333d2d3384f76268cb3e9f3d7564e9b9ad0c6f78
Luke Kolin <luke@deltava.org>
2011-06-27 19:30:27 +0000

    Add rewrite code
    git-svn-id: file:///home/svn/acars2006@7563 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/acars/beans/TestVoicePacket.java
+++ src/test/org/deltava/acars/beans/TestVoicePacket.java
@@ -1,17 +1,23 @@
 package org.deltava.acars.beans;
 
 import java.io.*;
+import java.util.Random;
+import java.util.zip.CRC32;
 
 import org.deltava.beans.Pilot;
+import org.deltava.beans.mvs.*;
+import org.deltava.beans.schedule.GeoPosition;
 
-import org.deltava.acars.beans.Packet.PacketInputStream;
+import org.deltava.acars.beans.Packet.*;
 import org.deltava.acars.message.VoiceMessage;
 
 import junit.framework.TestCase;
 
 public class TestVoicePacket extends TestCase {
 	
-	public void testLoadPackets() throws IOException {
+	private final Random _rnd = new Random();
+	
+	public void testCSPackets() throws IOException {
 		
 		File f = new File("c:\\temp\\test.mvs");
 		assertTrue(f.exists());
@@ -33,5 +39,35 @@ public class TestVoicePacket extends TestCase {
 		}
 		
 		ds.close();
+	}
+	
+	public void testJavaPackets() throws IOException {
+		
+		Pilot p = new Pilot("Luke", "Kollin");
+		p.setPilotCode("DVA043");
+		VoiceMessage vmsg = new VoiceMessage(p, "Lobby");
+		vmsg.setCompression(VoiceCompression.NONE);
+		vmsg.setRate(SampleRate.SR11K);
+		vmsg.setID(1);
+		vmsg.setLocation(new GeoPosition(33, -85.3));
+		
+		// Create payload
+		byte[] data = new byte[1280];
+		_rnd.nextBytes(data);
+		vmsg.setData(data);
+		
+		// Calculate CRC32
+		CRC32 crc = new CRC32();
+		crc.update(data);
+		vmsg.setCRC32(crc.getValue() & 0xFFFFFFFFL);
+		
+		// Now read the packet back
+		VoiceMessage vmsg2 = new VoiceMessage(vmsg.getSender(), vmsg.getChannel());
+		vmsg2.setData(Packet.rewrite(vmsg));
+		Packet.parse(vmsg2);
+		
+		// Compare
+		assertEquals(vmsg.getCompression(), vmsg2.getCompression());
+		assertEquals(vmsg.getRate(), vmsg2.getRate());
 	}
 }
