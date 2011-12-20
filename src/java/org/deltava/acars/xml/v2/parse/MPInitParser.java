395e1a3079a2837f15f53273d481aac078a9f43d
Luke Kolin <luke@deltava.org>
2011-12-20 03:01:20 +0000

    Multi-player adjustments
    git-svn-id: file:///home/svn/acars2006@7845 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/xml/v2/parse/MPInitParser.java
+++ src/java/org/deltava/acars/xml/v2/parse/MPInitParser.java
@@ -1,7 +1,9 @@
-// Copyright 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2010, 2011 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.xml.v2.parse;
 
 import org.deltava.beans.Pilot;
+import org.deltava.beans.schedule.GeoPosition;
+
 import org.deltava.util.StringUtils;
 
 import org.deltava.acars.message.mp.InitMessage;
@@ -11,7 +13,7 @@ import org.deltava.acars.xml.*;
 /**
  * A message parser for multi-player initialization messages. 
  * @author Luke
- * @version 3.0
+ * @version 4.1
  * @since 3.0
  */
 
@@ -23,11 +25,18 @@ class MPInitParser extends XMLElementParser<InitMessage> {
 	 * @return an MPInitMessage
 	 * @throws XMLException if a parse error occurs 
 	 */
+	@Override
 	public InitMessage parse(org.jdom.Element e, Pilot usr) {
 		
+		// Get the center
+		double lat = Double.parseDouble(e.getAttributeValue("lat", "0"));
+		double lng = Double.parseDouble(e.getAttributeValue("lon", "0"));
+
 		// Create the message
 		InitMessage msg = new InitMessage(usr);
+		msg.setLocation(new GeoPosition(lat, lng, StringUtils.parse(e.getAttributeValue("alt"), 0)));
 		msg.setRange(StringUtils.parse(getChildText(e, "range", "40"), 40));
+		msg.setLivery(getChildText(e, "livery", "DEFAULT"));
 		return msg;
 	}
 }
