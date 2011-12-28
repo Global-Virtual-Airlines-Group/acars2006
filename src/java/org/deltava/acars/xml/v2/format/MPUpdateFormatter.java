f5b8ad4a6ffdbc6f762ed36d74a7e9738a3e37c5
Luke Kolin <luke@deltava.org>
2011-12-28 17:13:49 +0000

    Rationalize IDs
    git-svn-id: file:///home/svn/acars2006@7867 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/xml/v2/format/MPUpdateFormatter.java
+++ src/java/org/deltava/acars/xml/v2/format/MPUpdateFormatter.java
@@ -46,8 +46,8 @@ public class MPUpdateFormatter extends XMLElementFormatter {
 			
 			// Build the element
 			Element le = new Element("pos");
-			le.setAttribute("id", String.valueOf(upd.getID())); // pilot's database ID!
-			le.setAttribute("flightID", String.valueOf(upd.getFlightID()));
+			le.setAttribute("id", upd.getHexID()); // pilot's database ID!
+			le.setAttribute("flightID", Integer.toHexString(upd.getFlightID()));
 			le.setAttribute("lat", StringUtils.format(lmsg.getLatitude(), "##0.000000"));
 			le.setAttribute("lon", StringUtils.format(lmsg.getLongitude(), "##0.000000"));
 			le.setAttribute("v", String.valueOf(lmsg.getVspeed()));
