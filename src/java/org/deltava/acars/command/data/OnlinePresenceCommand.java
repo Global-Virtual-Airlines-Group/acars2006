4c2fc1604967fc927812ccfb23824a948d1487c3
Luke Kolin <luke@deltava.org>
2012-01-06 15:52:28 +0000

    Add OnlinePresenceCommand
    git-svn-id: file:///home/svn/acars2006@7887 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/acars/command/data/OnlinePresenceCommand.java
@@ -0,0 +1,76 @@
+// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.acars.command.data;
+
+import java.io.*;
+
+import org.deltava.acars.beans.MessageEnvelope;
+import org.deltava.acars.command.*;
+import org.deltava.acars.message.*;
+
+import org.deltava.beans.OnlineNetwork;
+import org.deltava.beans.servinfo.*;
+
+import org.deltava.dao.file.GetServInfo;
+
+import org.deltava.util.system.SystemData;
+
+/**
+ * An ACARS command to determine whether a user is connected to an Online Network.
+ * @author Luke
+ * @version 4.1
+ * @since 4.1
+ */
+
+public class OnlinePresenceCommand extends DataCommand {
+
+	/**
+	 * Initializes the Command.
+	 */
+	public OnlinePresenceCommand() {
+		super(OnlinePresenceCommand.class);
+	}
+
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @param env the message Envelope
+	 */
+	@Override
+	public void execute(CommandContext ctx, MessageEnvelope env) {
+
+		// Get the message and the network
+		DataRequestMessage msg = (DataRequestMessage) env.getMessage();
+		OnlineNetwork network = null;
+		try {
+			network = OnlineNetwork.valueOf(msg.getFlag("network").toUpperCase());
+		} catch (IllegalArgumentException iae) {
+			log.warn("Unknown Online network - " + msg.getFlag("network"));
+		}
+		
+		// Create the response
+		AcknowledgeMessage ackMsg = new AcknowledgeMessage(env.getOwner(), msg.getID());
+		
+		// Get the data
+		if (network != null) {
+			try {
+				File f = new File(SystemData.get("online." + network.toString().toLowerCase() + ".local.info"));
+				if (f.exists()) {
+					GetServInfo sidao = new GetServInfo(new FileInputStream(f));
+					NetworkInfo info = sidao.getInfo(network);
+					
+					// Check for the user
+					for (Pilot p : info.getPilots()) {
+						if (p.getPilotID() == ctx.getUser().getID()) {
+							ackMsg.setEntry("isOnline", "true");
+							break;
+						}
+					}
+				}
+			} catch (Exception e) {
+				log.error("Cannot load " + network + " ServInfo feed - " + e.getMessage(), e);
+			}
+		}
+
+		ctx.push(ackMsg, ctx.getACARSConnection().getID());
+	}
+}
