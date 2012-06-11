7c91fdc364ff920ac93d5f9f46c421c82f2c1cb8
Luke Kolin <luke@deltava.org>
2012-06-11 21:13:25 +0000

    Added logging
    git-svn-id: file:///home/svn/acars2006@8105 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/FilePIREPCommand.java
+++ src/java/org/deltava/acars/command/FilePIREPCommand.java
@@ -211,6 +211,8 @@ public class FilePIREPCommand extends ACARSCommand {
 			Aircraft a = acdao.get(afr.getEquipmentType());
 			if (a == null) 
 				throw new DAOException("Invalid equipment type - " + afr.getEquipmentType());
+			else if (!a.getName().equals(afr.getEquipmentType()))
+				log.warn("Expected " + afr.getEquipmentType() + ", received " + a.getName());
 
 			// Check if this Flight Report counts for promotion
 			ctx.setMessage("Checking type ratings for " + ac.getUserID());
