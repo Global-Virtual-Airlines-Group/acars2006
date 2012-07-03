264ba2edda2eaed1c701e212b38fd5ed57b3efbe
Luke Kolin <luke@deltava.org>
2012-07-03 22:01:54 +0000

    Fix compiler warnings
    git-svn-id: file:///home/svn/acars2006@8142 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/ACARSCommand.java
+++ src/java/org/deltava/acars/command/ACARSCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2008, 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2008, 2011, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.command;
 
 import org.deltava.acars.beans.MessageEnvelope;
@@ -6,7 +6,7 @@ import org.deltava.acars.beans.MessageEnvelope;
 /**
  * An ACARS server command object.
  * @author Luke
- * @version 4.0
+ * @version 4.2
  * @since 1.0
  */
 
@@ -16,6 +16,7 @@ public abstract class ACARSCommand  {
 	 * Returns the maximum execution time of this command before a warning should be issued.
 	 * @return the maximum execution time in milliseconds
 	 */
+	@SuppressWarnings("all")
 	public int getMaxExecTime() {
 		return 1500;
 	}
