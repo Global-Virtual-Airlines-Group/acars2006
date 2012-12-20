fa8e67b69de473911d1fd9038143605860a52537
Luke Kolin <luke@deltava.org>
2012-12-20 03:24:36 +0000

    Gate support
    git-svn-id: file:///home/svn/acars2006@8608 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/dispatch/FlightDataCommand.java
+++ src/java/org/deltava/acars/command/dispatch/FlightDataCommand.java
@@ -17,24 +17,25 @@ import org.deltava.acars.message.dispatch.FlightDataMessage;
 /**
  * An ACARS server command to process Dispatch Messages.
  * @author Luke
- * @version 4.2
+ * @version 5.1
  * @since 1.1
  */
 
 public class FlightDataCommand extends DispatchCommand {
-	
+
 	/**
 	 * Initializes the Command.
 	 */
 	public FlightDataCommand() {
 		super(FlightDataCommand.class);
 	}
-	
+
 	/**
 	 * Executes the command.
 	 * @param ctx the Command context
 	 * @param env the message Envelope
 	 */
+	@Override
 	public void execute(CommandContext ctx, MessageEnvelope env) {
 
 		// Get the inbound message and the owner
@@ -45,10 +46,10 @@ public class FlightDataCommand extends DispatchCommand {
 			log.warn("Unauthorized dispatch message from " + env.getConnectionID());
 			return;
 		}
-		
+
 		// Create the ACK message
 		AcknowledgeMessage ackMsg = new AcknowledgeMessage(usr, msg.getID());
-		
+
 		// Check if this is saving a route
 		boolean isPlot = (msg.getRecipient() != null) && (msg.getRecipient().startsWith("$"));
 
@@ -61,17 +62,23 @@ public class FlightDataCommand extends DispatchCommand {
 				log.warn("Cannot send dispatch message to " + msg.getRecipient());
 			}
 		}
-		
-		// Save the dispatch message data
+
 		boolean canCreate = usr.isInRole("Route");
-		if ((msg.getRouteID() == 0) && canCreate && !msg.getNoSave()) {
-			try {
-				Connection c = ctx.getConnection();
-				
-				// Check for a duplicate
+		try {
+			Connection c = ctx.getConnection();
+			
+			// Populate the gates
+			GetGates gdao = new GetGates(c);
+			if (msg.getGateD() != null)
+				msg.setGateD(gdao.getGate(msg.getAirportD(), msg.getSimulator(), msg.getGateD().getName()));
+			if (msg.getGateA() != null)
+				msg.setGateA(gdao.getGate(msg.getAirportA(), msg.getSimulator(), msg.getGateA().getName()));
+
+			// Save the dispatch message data
+			if ((msg.getRouteID() == 0) && canCreate && !msg.getNoSave()) {
 				GetACARSRoute rdao = new GetACARSRoute(c);
 				int dupeID = rdao.hasDuplicate(msg.getAirportD(), msg.getAirportA(), msg.getRoute());
-				
+
 				// Write the route
 				if (dupeID == 0) {
 					SetRoute dao = new SetRoute(c);
@@ -80,24 +87,23 @@ public class FlightDataCommand extends DispatchCommand {
 					log.warn(con.getUser().getName() + " attempting to save duplicate of Route #" + dupeID);
 					msg.setRouteID(dupeID);
 				}
-			} catch (DAOException de) {
-				log.warn("Cannot save/update route data - " + de.getMessage(), de);
-			} finally {
-				ctx.release();
 			}
+		} catch (DAOException de) {
+			log.warn("Cannot save/update route data - " + de.getMessage(), de);
+		} finally {
+			ctx.release();
 		}
-		
+
 		// Send out the dispatch data
 		ackMsg.setEntry("routeID", String.valueOf(msg.getRouteID()));
 		ackMsg.setEntry("msgs", String.valueOf(dstC.size()));
 		if (!isPlot) {
-			for (Iterator<ACARSConnection> i = dstC.iterator(); i.hasNext(); ) {
-				ACARSConnection ac = i.next();
+			for (ACARSConnection ac : dstC) {
 				log.info("Dispatch info from " + usr.getPilotCode() + " to " + ac.getUserID());
 				ctx.push(msg, ac.getID());
 			}
 		}
-		
+
 		// Send out the ack
 		ctx.push(ackMsg, ctx.getACARSConnection().getID());
 	}
