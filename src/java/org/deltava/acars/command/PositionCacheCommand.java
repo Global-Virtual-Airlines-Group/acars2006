258f6d39890734e2b5c779740dc8408cf4f0318f
Luke Kolin <luke@deltava.org>
2017-05-17 02:02:41 +0000

    Add flush() and geolocation
    git-svn-id: file:///home/svn/acars2006@11655 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/PositionCacheCommand.java
+++ src/java/org/deltava/acars/command/PositionCacheCommand.java
@@ -7,49 +7,66 @@ import java.util.concurrent.locks.*;
 
 import org.deltava.acars.message.PositionMessage;
 import org.deltava.acars.util.PositionCache;
+import org.deltava.beans.schedule.Country;
 
 import org.deltava.dao.*;
+import org.deltava.util.cache.*;
+
 import org.deltava.dao.acars.SetPosition;
 import org.deltava.jdbc.ConnectionContext;
 
 /**
- * 
+ * An abstract class for ACARS commands to interact with the Position cache.
  * @author Luke
  * @version 7.3
  * @since 7.3
  */
 
 abstract class PositionCacheCommand extends ACARSCommand {
-
+	
 	private static final Lock w = new ReentrantLock();
-	private static final PositionCache _cache = new PositionCache(50, 15200);
+	private static final PositionCache _posCache = new PositionCache(50, 12500);
+	
+	private static final GeoCache<CacheableString> _cache = CacheManager.getGeo(CacheableString.class, "GeoCountry");
 
 	/**
 	 * Places a PositionMessage in the cache.
 	 * @param msg a PositionMessage
 	 */
 	protected static void queue(PositionMessage msg) {
-		_cache.queue(msg);
+		_posCache.queue(msg);
 	}
 	
 	/**
-	 * 
-	 * @param force
+	 * Looks
+	 * @param msg
+	 */
+	protected static void lookup(PositionMessage msg) {
+		CacheableString id = _cache.get(msg);
+		msg.setCountry((id == null) ? null : Country.get(id.getValue()));
+	}
+	
+	/**
+	 * Flushes the cache to the database.
+	 * @param force TRUE if the cache should be flushed ieven if not full, otherwise FALSE
 	 * @return the number of entries written
 	 */
 	protected static int flush(boolean force, ConnectionContext ctx) throws DAOException {
 		if (!w.tryLock())
 			return 0;
 
+		boolean doFree = !ctx.hasConnection();
 		try {
-			if (force || _cache.isFull()) {
-				Collection<PositionMessage> msgs = _cache.drain(); int entries = msgs.size();
+			if (force || _posCache.isFull()) {
+				Collection<PositionMessage> msgs = _posCache.drain(); int entries = msgs.size();
 				Connection con = ctx.getConnection();
 					
 				// Geolocation
 				GetCountry cdao = new GetCountry(con);
-				for (PositionMessage pmsg : msgs)
-					pmsg.setCountry(cdao.find(pmsg));
+				for (PositionMessage pmsg : msgs) {
+					if (pmsg.getCountry() != null)
+						pmsg.setCountry(cdao.find(pmsg));
+				}
 				
 				// Write the entries
 				SetPosition dao = new SetPosition(con);
@@ -57,8 +74,9 @@ abstract class PositionCacheCommand extends ACARSCommand {
 				return entries;
 			}
 		} finally {
-			ctx.release();
 			w.unlock();
+			if (doFree)
+				ctx.release();
 		}
 		
 		return 0;
