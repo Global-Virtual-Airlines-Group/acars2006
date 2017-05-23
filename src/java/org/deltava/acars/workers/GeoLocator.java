97adf46222f00bb626735c7c2f46fbb0ede337eb
Luke Kolin <luke@deltava.org>
2017-05-23 16:13:01 +0000

    Tweak caching/logging
    git-svn-id: file:///home/svn/acars2006@11680 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/workers/GeoLocator.java
+++ src/java/org/deltava/acars/workers/GeoLocator.java
@@ -4,11 +4,9 @@ package org.deltava.acars.workers;
 import java.util.*;
 import java.sql.Connection;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.LongAdder;
 
 import org.deltava.acars.beans.MessageEnvelope;
 import org.deltava.acars.message.PositionMessage;
-
 import org.deltava.beans.schedule.Country;
 
 import org.deltava.dao.*;
@@ -32,8 +30,8 @@ public class GeoLocator extends Worker {
 	private GeoCache<CacheableString> _l1c;
 	private GeoCache<CacheableString> _l2c;
 	
-	private final LongAdder _hits = new LongAdder();
-	private final LongAdder _reqs = new LongAdder();
+	private long _hits = 0;
+	private long _reqs = 0;
 
 	/**
 	 * Initializes the Worker.
@@ -62,31 +60,35 @@ public class GeoLocator extends Worker {
 		log.info("Started");
 		_status.setStatus(WorkerState.RUNNING);
 
-		Collection<PositionMessage> upds = new ArrayList<PositionMessage>();
+		Collection<PositionMessage> upds = new ArrayList<PositionMessage>(); long startTime = 0, execTime = 0;
 		while (!Thread.currentThread().isInterrupted()) {
-			_status.setMessage("Idle - " + _hits.longValue() + " / " + _reqs.longValue());
+			_status.setMessage("Idle - " + _hits + " hits / " + _reqs + " reqs");
 			
 			try {
-				Thread.sleep(10000); 			// Sleep for 10s
+				Thread.sleep(10000 - execTime); 			// Sleep for 10s
 
 				// Wait for data
 				MessageEnvelope env = GEO_INPUT.poll(20, TimeUnit.SECONDS);
+				startTime = System.currentTimeMillis();
 				_status.execute(); _status.setMessage("Loading Position Updates");
 				
 				// Get all of the updates
 				while (env != null) {
 					PositionMessage msg = (PositionMessage) env.getMessage();
-					_reqs.increment();
+					_reqs++;
 					
 					// GeoLocate if we can
 					CacheableString id = _l1c.get(msg);
-					if (id == null)
+					if (id == null) {
 						id = _l2c.get(msg);
+						if (id != null)
+							_l1c.add(msg, id);
+					}
 					
 					// If not, add it to the queue
 					if (id != null) {
 						msg.setCountry(Country.get(id.getValue()));
-						_hits.increment();
+						_hits++;
 					} else
 						upds.add(msg);
 					
@@ -101,7 +103,7 @@ public class GeoLocator extends Worker {
 						con = _jdbcPool.getConnection();
 						GetCountry cdao = new GetCountry(con);
 						for (PositionMessage msg : upds) {
-							Country c = cdao.find(msg); // This will populate L2 automatically
+							Country c = cdao.find(msg, true); // This will populate L2 automatically
 							_l1c.add(msg, new CacheableString("", c.getCode()));
 							msg.setCountry(c);
 						}
@@ -116,9 +118,10 @@ public class GeoLocator extends Worker {
 				Thread.currentThread().interrupt();
 			} catch (Exception e) {
 				log.error(e.getMessage(), e);
+			} finally {
+				execTime = Math.min(9750, System.currentTimeMillis() - startTime);
+				_status.complete();
 			}
-			
-			_status.complete();
 		}
 	}
 }
