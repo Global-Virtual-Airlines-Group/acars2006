f44745a5703a310f2bcd2d8ee18dea012e3fe8da
Luke Kolin <luke@deltava.org>
2019-05-13 15:44:44 +0000

    Add TaxiTimeCommand
    git-svn-id: file:///home/svn/acars2006@13433 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/acars/command/data/TaxiTimeCommand.java
@@ -0,0 +1,62 @@
+// Copyright 2019 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.acars.command.data;
+
+import org.deltava.acars.beans.MessageEnvelope;
+import org.deltava.acars.command.*;
+import org.deltava.acars.message.*;
+
+import org.deltava.beans.schedule.Airport;
+
+import org.deltava.dao.*;
+
+import org.deltava.util.system.SystemData;
+
+/**
+ * An ACARS command to return airport taxi times.
+ * @author Luke
+ * @version 8.6
+ * @since 8.6
+ */
+
+public class TaxiTimeCommand extends DataCommand {
+
+	/**
+	 * Initializes the Command.
+	 */
+	public TaxiTimeCommand() {
+		super(TaxiTimeCommand.class);
+	}
+
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @param env the message Envelope
+	 */
+	@Override
+	public void execute(CommandContext ctx, MessageEnvelope env) {
+		
+		// Get the message
+		DataRequestMessage msg = (DataRequestMessage) env.getMessage();
+		AcknowledgeMessage ackMsg = new AcknowledgeMessage(env.getOwner(), msg.getID());
+		
+		boolean isTakeoff = Boolean.valueOf(msg.getFlag("isTakeoff")).booleanValue();
+		Airport a = SystemData.getAirport(msg.getFlag("airport"));
+		if (a == null) {
+			ackMsg.setEntry("error", "Unknown Airport - " + msg.getFlag("airport"));
+			ctx.push(ackMsg);
+			return;
+		}
+		
+		String db = ctx.getACARSConnection().getUserData().getDB();
+		try {
+			GetACARSTaxiTimes ttdao = new GetACARSTaxiTimes(ctx.getConnection());
+			int taxiTime = isTakeoff ? ttdao.getTaxiOutTime(a, db) : ttdao.getTaxiInTime(a, db);
+			ackMsg.setEntry("taxiTime", String.valueOf(taxiTime));
+			ctx.push(ackMsg);
+		} catch (DAOException de) {
+			log.error("Error loading taxi times - " + de.getMessage(), de);
+		} finally {
+			ctx.release();
+		}
+	}
+}
