7e8964db0a535bd8e31dea7eeff21a30bb6a6efe
Luke Kolin <luke@deltava.org>
2019-07-01 20:17:59 +0000

    Ground operations GSX2 support
    git-svn-id: file:///home/svn/acars2006@13625 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/acars/SetPosition.java
+++ src/java/org/deltava/dao/acars/SetPosition.java
@@ -1,4 +1,4 @@
-// Copyright 2004, 2005, 2006, 2007, 2010, 2012, 2014, 2016, 2017, 2018 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2004, 2005, 2006, 2007, 2010, 2012, 2014, 2016, 2017, 2018, 2019 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.acars;
 
 import java.sql.*;
@@ -11,7 +11,7 @@ import org.deltava.acars.message.PositionMessage;
 /**
  * A Data Access Object to write ACARS Position Messages.
  * @author Luke
- * @version 8.3
+ * @version 8.6
  * @since 1.0
  */
 
@@ -32,9 +32,9 @@ public class SetPosition extends DAO {
 	 */
 	public void flush(Collection<PositionMessage> entries) throws DAOException {
 		try {
-			prepareStatementWithoutLimits("REPLACE INTO acars.POSITIONS (FLIGHT_ID, REPORT_TIME, SIM_TIME, LAT, LNG, B_ALT, R_ALT, HEADING, ASPEED, GSPEED, VSPEED, "
-				+ "N1, N2, MACH, FUEL, PHASE, SIM_RATE, FLAGS, FLAPS, PITCH, BANK, FUELFLOW, WIND_HDG, WIND_SPEED, TEMP, PRESSURE, VIZ, AOA, GFORCE, FRAMERATE, "
-				+ "NAV1, NAV2, VAS, WEIGHT, ASTYPE, ADF1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			prepareStatementWithoutLimits("REPLACE INTO acars.POSITIONS (FLIGHT_ID, REPORT_TIME, SIM_TIME, LAT, LNG, B_ALT, R_ALT, HEADING, ASPEED, GSPEED, VSPEED, N1, N2, MACH, "
+				+ "FUEL, PHASE, SIM_RATE, FLAGS, GNDFLAGS, FLAPS, PITCH, BANK, FUELFLOW, WIND_HDG, WIND_SPEED, TEMP, PRESSURE, VIZ, AOA, GFORCE, FRAMERATE, NAV1, NAV2, VAS, "
+				+ "WEIGHT, ASTYPE, ADF1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 			
 			// Write core entries
 			startTransaction();
@@ -60,24 +60,25 @@ public class SetPosition extends DAO {
 				_ps.setInt(16, msg.getPhase().ordinal());
 				_ps.setInt(17, msg.getSimRate());
 				_ps.setInt(18, msg.getFlags());
-				_ps.setInt(19, msg.getFlaps());
-				_ps.setDouble(20, msg.getPitch());
-				_ps.setDouble(21, msg.getBank());
-				_ps.setInt(22, msg.getFuelFlow());
-				_ps.setInt(23, msg.getWindHeading());
-				_ps.setInt(24, msg.getWindSpeed());
-				_ps.setInt(25, msg.getTemperature());
-				_ps.setInt(26, msg.getPressure());
-				_ps.setDouble(27, msg.getVisibility());
-				_ps.setDouble(28, msg.getAngleOfAttack());
-				_ps.setDouble(29, msg.getG());
-				_ps.setInt(30, msg.getFrameRate());
-				_ps.setString(31, msg.getNAV1());
-				_ps.setString(32, msg.getNAV2());
-				_ps.setInt(33, msg.getVASFree());
-				_ps.setInt(34, msg.getWeight());
-				_ps.setInt(35, msg.getAirspaceType().ordinal());
-				_ps.setString(36, msg.getADF1());
+				_ps.setInt(19, msg.getGroundOperations());
+				_ps.setInt(20, msg.getFlaps());
+				_ps.setDouble(21, msg.getPitch());
+				_ps.setDouble(22, msg.getBank());
+				_ps.setInt(23, msg.getFuelFlow());
+				_ps.setInt(24, msg.getWindHeading());
+				_ps.setInt(25, msg.getWindSpeed());
+				_ps.setInt(26, msg.getTemperature());
+				_ps.setInt(27, msg.getPressure());
+				_ps.setDouble(28, msg.getVisibility());
+				_ps.setDouble(29, msg.getAngleOfAttack());
+				_ps.setDouble(30, msg.getG());
+				_ps.setInt(31, msg.getFrameRate());
+				_ps.setString(32, msg.getNAV1());
+				_ps.setString(33, msg.getNAV2());
+				_ps.setInt(34, msg.getVASFree());
+				_ps.setInt(35, msg.getWeight());
+				_ps.setInt(36, msg.getAirspaceType().ordinal());
+				_ps.setString(37, msg.getADF1());
 				_ps.addBatch();
 				
 				// Remove entries with no ATC ID
