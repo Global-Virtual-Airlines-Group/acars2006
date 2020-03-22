64cc576e35c08344e145109a295222a8352298a6
Luke Kolin <luke@deltava.org>
2020-03-22 13:37:40 +0000

    Fix compiler warnings
    git-svn-id: file:///home/svn/acars2006@14470 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/acars/mvs/TestEcho.java
+++ src/test/org/deltava/acars/mvs/TestEcho.java
@@ -12,7 +12,6 @@ import org.apache.log4j.*;
 import org.deltava.beans.mvs.*;
 import org.deltava.beans.schedule.GeoPosition;
 import org.deltava.util.*;
-import org.deltava.acars.mvs.Packet;
 
 import junit.framework.TestCase;
 
