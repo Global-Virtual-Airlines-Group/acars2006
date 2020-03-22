72c07e1a95c43badd65032512b279930ef51db5f
Luke Kolin <luke@deltava.org>
2020-03-22 13:42:29 +0000

    Fix compiler warnings
    git-svn-id: file:///home/svn/acars2006@14472 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/xml/XMLElementFormatter.java
+++ src/java/org/deltava/acars/xml/XMLElementFormatter.java
@@ -1,17 +1,16 @@
-// Copyright 2006, 2007, 2009, 2011, 2012, 2018 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2007, 2009, 2011, 2012, 2018, 2020 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.xml;
 
 import org.jdom2.Element;
 
 import org.deltava.acars.message.*;
-import org.deltava.acars.xml.ProtocolInfo;
 
 import org.deltava.util.*;
 
 /**
  * A formatter to create XML command elements.
  * @author Luke
- * @version 8.4
+ * @version 9.0
  * @since 1.0
  */
 
@@ -26,6 +25,7 @@ public abstract class XMLElementFormatter {
 	
 	/**
 	 * Helper method to initialize the response element.
+	 * @param msgType the MessageType
 	 */
 	protected static Element initResponse(MessageType msgType) {
 		Element e = new Element(ProtocolInfo.CMD_ELEMENT_NAME);
@@ -35,15 +35,18 @@ public abstract class XMLElementFormatter {
 	
 	/**
 	 * Helper method to initialize a DataResponse element.
+	 * @param cmd the parent XML element
+	 * @param rspType the response type name 
 	 */
 	protected static Element initDataResponse(Element cmd, String rspType) {
+		
 		// Get the element
 		Element e = cmd.getChild(rspType);
 		if (e != null)
 			return e;
 
 		// Create the new element
-		cmd.addContent(XMLUtils.createElement("rsptype", rspType));
+		cmd.addContent(XMLUtils.createElement("rsptype", rspType, false));
 		e = new Element(rspType);
 		cmd.addContent(e);
 		return e;
