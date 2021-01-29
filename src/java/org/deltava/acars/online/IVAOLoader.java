4670be7e00fca64dacbb7876c140c5d991fb54e1
Luke Kolin <luke@deltava.org>
2021-01-29 20:24:24 +0000

    Refactor loader hierarchy
    git-svn-id: file:///home/svn/acars2006@15709 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/online/IVAOLoader.java
+++ src/java/org/deltava/acars/online/IVAOLoader.java
@@ -1,14 +1,13 @@
-// Copyright 2020 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2020, 2021 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.online;
 
 import java.io.*;
 import java.util.Properties;
 
-import org.apache.log4j.Logger;
-
 import org.deltava.beans.OnlineNetwork;
 import org.deltava.beans.servinfo.NetworkInfo;
 
+import org.deltava.dao.DAOException;
 import org.deltava.dao.file.GetServInfo;
 import org.deltava.dao.http.GetURL;
 
@@ -17,50 +16,41 @@ import org.deltava.util.system.SystemData;
 /**
  * A network information loader for IVAO.
  * @author Luke
- * @version 9.2
+ * @version 9.1
  * @since 9.0
  */
 
 public class IVAOLoader extends Loader {
 	
-	private static final Logger log = Logger.getLogger(IVAOLoader.class);
-
+	/**
+	 * Creates the Loader.
+	 */
 	public IVAOLoader() {
 		super(OnlineNetwork.IVAO, 60);
 	}
 
 	@Override
-	public void run() {
-		super.run();
-		String url = null;
-		try {
-			GetURL urldao = new GetURL(SystemData.get("online.ivao.status_url"), SystemData.get("online.ivao.local.status"));
-			urldao.setConnectTimeout(3500);
-			urldao.setReadTimeout(4500);
-			Properties p = new Properties();
-			File f = urldao.download();
-			try (InputStream is = new FileInputStream(f)) {
-				p.load(is);
-			}
+	public void execute() throws IOException, DAOException {
 
-			urldao = new GetURL(p.getProperty("url0"), SystemData.get("online.ivao.local.info"));
-			urldao.setConnectTimeout(3500);
-			urldao.setReadTimeout(25000);
-			f = urldao.download();
-			try (InputStream is = new BufferedInputStream(new FileInputStream(f), 65536)) {
-				GetServInfo sidao = new GetServInfo(is, _network);
-				NetworkInfo in = sidao.getInfo();
-				if ((in != null) && in.getValidDate().isAfter(getLastUpdate())) {
-					update(in);
-					f.setLastModified(in.getValidDate().toEpochMilli());
-				}
+		GetURL urldao = new GetURL(SystemData.get("online.ivao.status_url"), SystemData.get("online.ivao.local.status"));
+		urldao.setConnectTimeout(3500);
+		urldao.setReadTimeout(4500);
+		Properties p = new Properties();
+		try (InputStream is = new FileInputStream(urldao.download())) {
+			p.load(is);
+		}
+
+		urldao = new GetURL(p.getProperty("url0"), SystemData.get("online.ivao.local.info"));
+		urldao.setConnectTimeout(4500);
+		urldao.setReadTimeout(25000);
+		File f = urldao.download();
+		try (InputStream is = new BufferedInputStream(new FileInputStream(f), 65536)) {
+			GetServInfo sidao = new GetServInfo(is, _network);
+			NetworkInfo in = sidao.getInfo();
+			if ((in != null) && in.getValidDate().isAfter(getLastUpdate())) {
+				update(in);
+				f.setLastModified(in.getValidDate().toEpochMilli());
 			}
-		} catch (Exception e) {
-			boolean isTimeout = e.getCause() instanceof java.net.SocketTimeoutException;
-			if (isTimeout)
-				log.warn("Timeout loading " + url);
-			else
-				log.error(e.getMessage(), e);
 		}
 	}
 }
--- src/java/org/deltava/acars/online/VATSIMLoader.java
+++ src/java/org/deltava/acars/online/VATSIMLoader.java
@@ -1,13 +1,12 @@
-// Copyright 2020 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2020, 2021 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.online;
 
 import java.io.*;
 
-import org.apache.log4j.Logger;
-
 import org.deltava.beans.OnlineNetwork;
 import org.deltava.beans.servinfo.NetworkInfo;
 
+import org.deltava.dao.DAOException;
 import org.deltava.dao.file.GetVATSIMInfo;
 import org.deltava.dao.http.GetURL;
 
@@ -22,30 +21,28 @@ import org.deltava.util.system.SystemData;
 
 public class VATSIMLoader extends Loader {
 	
-	private static final Logger log = Logger.getLogger(VATSIMLoader.class);
-
+	/**
+	 * Creates the Loader.
+	 */
 	public VATSIMLoader() {
 		super(OnlineNetwork.VATSIM, 30);
 	}
 
 	@Override
-	public void run() {
-		super.run();
-		try {
-			GetURL urldao = new GetURL(SystemData.get("online.vatsim.status_url"), SystemData.get("online.vatsim.local.info"));
-			urldao.setConnectTimeout(5000);
-			urldao.setReadTimeout(15000);
-			File f = urldao.download();
-			try (InputStream is = new BufferedInputStream(new FileInputStream(f), 131072)) {
-				GetVATSIMInfo sidao = new GetVATSIMInfo(is);
-				NetworkInfo in = sidao.getInfo();
-				if ((in != null) && in.getValidDate().isAfter(getLastUpdate())) {
-					update(in);
-					f.setLastModified(in.getValidDate().toEpochMilli());
-				}
+	public void execute() throws IOException, DAOException {
+
+		GetURL urldao = new GetURL(SystemData.get("online.vatsim.status_url"), SystemData.get("online.vatsim.local.info"));
+		urldao.setConnectTimeout(5000);
+		urldao.setReadTimeout(15000);
+		File f = urldao.download();
+		try (InputStream is = new BufferedInputStream(new FileInputStream(f), 131072)) {
+			GetVATSIMInfo sidao = new GetVATSIMInfo(is);
+			sidao.setVersion(2);
+			NetworkInfo in = sidao.getInfo();
+			if ((in != null) && in.getValidDate().isAfter(getLastUpdate())) {
+				update(in);
+				f.setLastModified(in.getValidDate().toEpochMilli());
 			}
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
 		}
 	}
 }
