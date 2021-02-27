12d705f69c4670e6e38526b047a2db620311bdad
Luke Kolin <luke@deltava.org>
2021-02-27 15:36:25 +0000

    Add FlightSubmissionHelper
    git-svn-id: file:///home/svn/acars2006@15808 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/acars/util/FlightInfoHelper.java
@@ -0,0 +1,50 @@
+// Copyright 2021 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.acars.util;
+
+import org.deltava.acars.message.InfoMessage;
+import org.deltava.beans.acars.FlightInfo;
+
+/**
+ * A utility class to convert FlightInfo messages to Flight Information beans.
+ * @author Luke
+ * @version 10.0
+ * @since 10.0
+ */
+
+public class FlightInfoHelper {
+
+	// static class
+	private FlightInfoHelper() {
+		super();
+	}
+	
+	/**
+	 * Converts an InfoMessage to a FlightInfo bean.
+	 * @param msg an InfoMessage
+	 * @return a FlightInfo bean
+	 */
+	public static FlightInfo convert(InfoMessage msg) {
+
+		FlightInfo inf = new FlightInfo(msg.getFlightID());
+		inf.setAirportD(msg.getAirportD());
+		inf.setAirportA(msg.getAirportA());
+		inf.setAirportL(msg.getAirportL());
+		inf.setFlightCode(msg.getFlightCode());
+		inf.setScheduleValidated(msg.isScheduleValidated());
+		inf.setAltitude(msg.getAltitude());
+		inf.setEquipmentType(msg.getEquipmentType());
+		inf.setStartTime(msg.getStartTime());
+		inf.setEndTime(msg.getEndTime());
+		inf.setLoadType(msg.getLoadType());
+		inf.setLoadFactor(msg.getLoadFactor());
+		inf.setAutopilotType(msg.getAutopilotType());
+		inf.setDispatcherID(msg.getDispatcherID());
+		inf.setDispatchLogID(msg.getDispatchLogID());
+		inf.setDispatchPlan(msg.isDispatchPlan());
+		inf.setIsACARS64Bit(msg.getIsACARS64Bit());
+		inf.setIsSim64Bit(msg.getIsSim64Bit());
+		inf.setPassengers(msg.getPassengers());
+		inf.setSimulator(msg.getSimulator());
+		return inf;
+	}
+}
