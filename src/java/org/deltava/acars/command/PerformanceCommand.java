147fe5132638f932f1bc594b9509269cf0c5198d
Luke Kolin <luke@deltava.org>
2021-04-04 14:52:25 +0000

    Avoid foreign key errors and return to client
    git-svn-id: file:///home/svn/acars2006@16140 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/PerformanceCommand.java
+++ src/java/org/deltava/acars/command/PerformanceCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2019 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2019, 2021 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.command;
 
 import org.apache.log4j.Logger;
@@ -12,7 +12,7 @@ import org.deltava.dao.acars.SetSystemInfo;
 /**
  * An ACARS server command to handle client performance counter messages.
  * @author Luke
- * @version 8.6
+ * @version 10.0
  * @since 8.6
  */
 
@@ -29,6 +29,13 @@ public class PerformanceCommand extends ACARSCommand {
 	public void execute(CommandContext ctx, MessageEnvelope env) {
 		
 		PerformanceMessage msg = (PerformanceMessage) env.getMessage();
+		InfoMessage info = ctx.getACARSConnection().getFlightInfo();
+		if ((info == null) || (info.getFlightID() != msg.getFlightID())) {
+			int flightID = (info == null) ? 0 : info.getFlightID();
+			ctx.push(new AcknowledgeMessage(ctx.getUser(), msg.getID(), String.format("Flight ID %d does not match Metrics ID %d", Integer.valueOf(flightID), Integer.valueOf(msg.getFlightID()))));
+			return;
+		}
+		
 		try {
 			SetSystemInfo dao = new SetSystemInfo(ctx.getConnection());
 			dao.write(msg);
