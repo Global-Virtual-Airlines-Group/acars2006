b80cf50fabe5f0612e8bccb2452c47d60605bf80
Luke Kolin <luke@deltava.org>
2021-04-08 19:04:05 +0000

    Better linking to flight ID
    git-svn-id: file:///home/svn/acars2006@16161 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/PerformanceCommand.java
+++ src/java/org/deltava/acars/command/PerformanceCommand.java
@@ -1,12 +1,16 @@
 // Copyright 2019, 2021 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.command;
 
+import java.sql.Connection;
+
 import org.apache.log4j.Logger;
 
 import org.deltava.acars.message.*;
 
+import org.deltava.beans.acars.FlightInfo;
 import org.deltava.acars.beans.MessageEnvelope;
-import org.deltava.dao.DAOException;
+
+import org.deltava.dao.*;
 import org.deltava.dao.acars.SetSystemInfo;
 
 /**
@@ -30,16 +34,22 @@ public class PerformanceCommand extends ACARSCommand {
 		
 		PerformanceMessage msg = (PerformanceMessage) env.getMessage();
 		InfoMessage info = ctx.getACARSConnection().getFlightInfo();
-		if ((info == null) || (info.getFlightID() != msg.getFlightID())) {
-			int flightID = (info == null) ? 0 : info.getFlightID();
-			ctx.push(new AcknowledgeMessage(ctx.getUser(), msg.getID(), String.format("Flight ID %d does not match Metrics ID %d", Integer.valueOf(flightID), Integer.valueOf(msg.getFlightID()))));
-			return;
-		}
-		
+		boolean idOK = (info != null) && (info.getFlightID() != msg.getFlightID());
 		try {
-			SetSystemInfo dao = new SetSystemInfo(ctx.getConnection());
+			Connection con = ctx.getConnection();
+			if (!idOK) {
+				GetACARSData idao = new GetACARSData(con);
+				FlightInfo inf = idao.getInfo(msg.getFlightID());
+				idOK = (inf != null) && (inf.getID() == msg.getFlightID());
+				if (!idOK)
+					throw new IllegalArgumentException(String.format("Flight ID %d not owned by %s", Integer.valueOf(msg.getFlightID()), env.getOwner().getPilotCode()));
+			}
+			
+			SetSystemInfo dao = new SetSystemInfo(con);
 			dao.write(msg);
 			ctx.push(new AcknowledgeMessage(ctx.getUser(), msg.getID()));
+		} catch (IllegalArgumentException iae) {
+			ctx.push(new AcknowledgeMessage(ctx.getUser(), msg.getID(), iae.getMessage()));
 		} catch (DAOException de) {
 			log.error(de.getMessage(), de);
 			ctx.push(new AcknowledgeMessage(ctx.getUser(), msg.getID(), de.getMessage()));
