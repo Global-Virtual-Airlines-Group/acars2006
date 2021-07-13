f87615cbad7d3edad019d43fa1625f9fc9f9bcb8
Luke Kolin <luke@deltava.org>
2021-07-13 12:19:22 +0000

    IVAO v2 JSON feed loader
    git-svn-id: file:///home/svn/acars2006@16545 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/online/IVAOLoader.java
+++ src/java/org/deltava/acars/online/IVAOLoader.java
@@ -2,13 +2,12 @@
 package org.deltava.acars.online;
 
 import java.io.*;
-import java.util.Properties;
 
 import org.deltava.beans.OnlineNetwork;
 import org.deltava.beans.servinfo.NetworkInfo;
 
 import org.deltava.dao.DAOException;
-import org.deltava.dao.file.GetServInfo;
+import org.deltava.dao.file.GetIVAOInfo;
 import org.deltava.dao.http.DAO.Compression;
 import org.deltava.dao.http.GetURL;
 
@@ -17,7 +16,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A network information loader for IVAO.
  * @author Luke
- * @version 10.0
+ * @version 10.1
  * @since 9.0
  */
 
@@ -27,27 +26,19 @@ public class IVAOLoader extends Loader {
 	 * Creates the Loader.
 	 */
 	public IVAOLoader() {
-		super(OnlineNetwork.IVAO, 60);
+		super(OnlineNetwork.IVAO, 45);
 	}
 
 	@Override
 	public void execute() throws IOException, DAOException {
 
-		GetURL urldao = new GetURL(SystemData.get("online.ivao.status_url"), SystemData.get("online.ivao.local.status"));
-		urldao.setConnectTimeout(3500);
-		urldao.setReadTimeout(4500);
-		urldao.setCompression(Compression.GZIP);
-		Properties p = new Properties();
-		try (InputStream is = new FileInputStream(urldao.download())) {
-			p.load(is);
-		}
-
-		urldao = new GetURL(p.getProperty("url0"), SystemData.get("online.ivao.local.info"));
+		GetURL urldao = new GetURL(SystemData.get("online.ivao.status_url"), SystemData.get("online.ivao.local.info"));
 		urldao.setConnectTimeout(4500);
 		urldao.setReadTimeout(25000);
+		urldao.setCompression(Compression.GZIP);
 		File f = urldao.download();
-		try (InputStream is = new BufferedInputStream(new FileInputStream(f), 65536)) {
-			GetServInfo sidao = new GetServInfo(is, _network);
+		try (InputStream is = new BufferedInputStream(new FileInputStream(f), 131072)) {
+			GetIVAOInfo sidao = new GetIVAOInfo(is);
 			NetworkInfo in = sidao.getInfo();
 			if ((in != null) && in.getValidDate().isAfter(getLastUpdate())) {
 				update(in);
