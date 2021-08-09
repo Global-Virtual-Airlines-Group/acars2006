23afff7ca5ed7e4734f336c9ea1b835cd994c0d5
Luke Kolin <luke@deltava.org>
2021-08-09 21:00:51 +0000

    Include submitted flight reports
    git-svn-id: file:///home/svn/acars2006@16697 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/acars/command/data/LastAirportCommand.java
+++ src/java/org/deltava/acars/command/data/LastAirportCommand.java
@@ -28,17 +28,15 @@ public class LastAirportCommand extends DataCommand {
 	@Override
 	public void execute(CommandContext ctx, MessageEnvelope env) {
 
-		// Create the acknowledgement
 		AcknowledgeMessage ackMsg = new AcknowledgeMessage(ctx.getUser(), env.getMessage().getID());
-
-		LogbookSearchCriteria sc = new LogbookSearchCriteria("SUBMITTED DESC", ctx.getDB());
 		try {
 			GetFlightReports frdao = new GetFlightReports(ctx.getConnection());
 			frdao.setQueryMax(15);
 			
 			// Load all PIREPs and save the latest PIREP as a separate bean in the request
+			LogbookSearchCriteria sc = new LogbookSearchCriteria("SUBMITTED DESC", ctx.getDB());
 			List<FlightReport> results = frdao.getByPilot(ctx.getUser().getID(), sc);
-			Optional<FlightReport> ofr = results.stream().filter(f -> ((f.getStatus() == FlightStatus.OK) || (f.getStatus() == FlightStatus.HOLD))).findFirst();
+			Optional<FlightReport> ofr = results.stream().filter(f -> ((f.getStatus() != FlightStatus.DRAFT) && (f.getStatus() != FlightStatus.REJECTED))).findFirst();
 			if (ofr.isPresent())
 				ackMsg.setEntry("lastAirport", ofr.get().getAirportA().getICAO());
 		} catch (DAOException de) {
