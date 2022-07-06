85819be93bfac4af1cca5ac2d51b6caa0996baf0
Luke Kolin <luke@deltava.org>
2022-07-06 13:30:15 +0000

    ACARS Bandwidth JMX beans
    git-svn-id: file:///home/svn/acars2006@17519 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/acars/jmx/BandwidthMBean.java
@@ -0,0 +1,69 @@
+// Copyright 2021 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.acars.jmx;
+
+import javax.management.MXBean;
+
+/**
+ * A JMX MBean for ACARS bandwidth statistics. 
+ * @author Luke
+ * @version 10.2
+ * @since 10.2
+ */
+
+@MXBean
+public interface BandwidthMBean {
+
+	/**
+	 * Returns the inbound bandwidth during the time period.
+	 * @return the inbound number of bytes
+	 */
+	Long getBytesIn();
+	
+	/**
+	 * Returns the outbound bandwidth during the time period.
+	 * @return the outbound number of bytes
+	 */
+	Long getBytesOut();
+	
+	/**
+	 * Returns the bandwidth saved via compression during the time period.
+	 * @return the reduced number of bytes
+	 */
+	Long getBytesSaved();
+	
+	/**
+	 * Returns the number of ACARS connections.
+	 * @return the number of connections
+	 */
+	Integer getConnections();
+
+	/**
+	 * Returns the number of inbound ACARS protocol messages during the time period.
+	 * @return the inbound number of messages
+	 */
+	Integer getMsgsIn();
+	
+	/**
+	 * Returns the number of outbound ACARS protocol messages during the time period.
+	 * @return the outbound number of messages
+	 */
+	Integer getMsgsOut();
+	
+	/**
+	 * Returns the maximum number of ACARS connections during the time period.
+	 * @return the maximum number of connections
+	 */
+	Integer getMaxConnections();
+
+	/**
+	 * Returns the maximum amount of bandwidth during the time period.
+	 * @return the maximum number of bytes
+	 */
+	Long getMaxBytes();
+
+	/**
+	 * Returns the maximum number of outbound bandwidth during the time period.
+	 * @return the maximum outbound number of bytes
+	 */
+	Integer getMaxMsgs();
+}
---
+++ src/java/org/deltava/acars/jmx/BandwidthMBeanImpl.java
@@ -0,0 +1,78 @@
+// Copyright 2021 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.acars.jmx;
+
+import org.deltava.beans.acars.Bandwidth;
+
+/**
+ * A JMX MBean implementation for ACARS bandwidth statistics. 
+ * @author Luke
+ * @version 10.2
+ * @since 10.2
+ */
+
+public class BandwidthMBeanImpl implements BandwidthMBean {
+	
+	private Bandwidth _b;
+
+	/**
+	 * Creates the bean.
+	 * @param b a Bandwidth bean 
+	 */
+	public BandwidthMBeanImpl(Bandwidth b) {
+		super();
+		_b = b;
+	}
+	
+	/**
+	 * Updates the bandwidth statistics.
+	 * @param b a Bandwidth bean
+	 */
+	public void update(Bandwidth b) {
+		_b = b;
+	}
+
+	@Override
+	public Long getBytesIn() {
+		return Long.valueOf(_b.getBytesIn());
+	}
+
+	@Override
+	public Long getBytesOut() {
+		return Long.valueOf(_b.getBytesOut());
+	}
+
+	@Override
+	public Long getBytesSaved() {
+		return Long.valueOf(_b.getBytesSaved());
+	}
+
+	@Override
+	public Integer getConnections() {
+		return Integer.valueOf(_b.getConnections());
+	}
+
+	@Override
+	public Integer getMsgsIn() {
+		return Integer.valueOf(_b.getMsgsIn());
+	}
+
+	@Override
+	public Integer getMsgsOut() {
+		return Integer.valueOf(_b.getMsgsOut());
+	}
+
+	@Override
+	public Integer getMaxConnections() {
+		return Integer.valueOf(_b.getMaxConnections());
+	}
+
+	@Override
+	public Long getMaxBytes() {
+		return Long.valueOf(_b.getMaxBytes());
+	}
+
+	@Override
+	public Integer getMaxMsgs() {
+		return Integer.valueOf(_b.getMaxMsgs());
+	}
+}
