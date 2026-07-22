c72a1234ba769dfadc9d7252517dbe082d0fa673
Luke Kolin <luke@deltava.org>
2026-07-22 10:44:11 -0400

    Send ACK even if unchanged

--- src/java/org/deltava/acars/command/CompressionCommand.java
+++ src/java/org/deltava/acars/command/CompressionCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2015, 2016, 2019, 2023 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2015, 2016, 2019, 2023, 2026 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.acars.command;
 
 import org.apache.logging.log4j.*;
@@ -9,7 +9,7 @@ import org.deltava.acars.message.*;
 /**
  * An ACARS Server Command to enable/disable data compression. 
  * @author Luke
- * @version 11.1
+ * @version 12.5
  * @since 6.4
  */
 
@@ -28,17 +28,14 @@ public class CompressionCommand extends ACARSCommand {
 		// Get the message and the connection
 		ACARSConnection ac = ctx.getACARSConnection();
 		final CompressionMessage msg = (CompressionMessage) env.getMessage(); 
-		String cType = msg.getCompression().name().toLowerCase(); 
-		if (ac.getCompression() == msg.getCompression()) {
-			log.info("{} requesting {} compression, already set", ac.getUserID(), cType);
-			return;
-		}
+		String cType = msg.getCompression().name().toLowerCase();
+		if (ac.getCompression() != msg.getCompression()) {
+			log.info("Setting data compression for {} to {}", ac.getUserID(), cType);
+			ac.setCompression(msg.getCompression());
+		} else
+			log.warn("{} requesting {} compression, already set", ac.getUserID(), cType);
 		
-		// Set compression for connection
-		log.info("Setting data compression for {} to {}", ac.getUserID(), cType);
-		ac.setCompression(msg.getCompression());
-
-		// Send an ACK
+		// Send the ACK
 		AcknowledgeMessage ackMsg = new AcknowledgeMessage(env.getOwner(), msg.getID());
 		ackMsg.setEntry("type", cType);
 		ctx.push(ackMsg);
